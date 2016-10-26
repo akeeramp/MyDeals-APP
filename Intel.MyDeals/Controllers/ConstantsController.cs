c929edf7cf665e3109cd9aa19950b00f5bd6d997
Biradar, Mahesh <mahesh.biradar@intel.com>
2016-10-26 04:24:14 +0000

    Adding constants controller
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1394374

---
+++ Intel.MyDeals/Controllers/ConstantsController.cs
@@ -0,0 +1,24 @@
+﻿using Intel.MyDeals.App;
+using Intel.Opaque;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intel.MyDeals.Controllers
+{
+    public class ConstantsController : Controller
+    {
+        OpCore op = OpAppConfig.Init();
+
+        // GET: Constants
+        public ActionResult Index()
+        {
+            OpUserToken user = AppLib.InitAVM(op);
+            ViewBag.UserToken = user;
+            ViewBag.AppToken = op.AppToken;
+            return View();
+        }
+    }
+}
---
+++ Intel.MyDeals/js/constantsController.js
@@ -0,0 +1,34 @@
+﻿var app = angular.module('MyDealsApp', ['kendo.directives']);
+
+app.controller('constantsController', ["$scope", constantsController]);
+function constantsController($scope) {
+    // declare dataSource bound to backend
+    var dataSource = new kendo.data.DataSource({
+        transport: {
+            read: {
+                async: false,
+                url: 'api/Constants/v1/GetConstants',
+                dataType: "json"
+            }
+        },
+        toolbar: ["create"],
+        serverPaging: false,
+        pageSize: 10,
+        error: function (e) {
+            alert(e.xhr.responseText);
+        },
+        editable: true
+    });
+
+
+    $scope.gridOptions = {
+        dataSource: dataSource,
+        sortable: true,
+        pageable: true,
+        columns: [
+          { field: "CNST_NM", title: "Name" },
+          { field: "CNST_VAL_TXT", title: "Value"},
+          { field: "CNST_DESC", title: "Description" },
+        ]
+    };
+}
