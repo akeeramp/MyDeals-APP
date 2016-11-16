0dece0b63bb129aca9bbdcb2b9f444865f061b57
Biradar, Mahesh <mahesh.biradar@intel.com>
2016-11-16 05:04:02 +0000

    build failure fix
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1398428

--- Intel.MyDeals.DataLibrary.Test/OtherTests.cs
+++ Intel.MyDeals.DataLibrary.Test/OtherTests.cs
@@ -17,7 +17,7 @@ namespace Intel.MyDeals.DataLibrary.Test
         [TestMethod]
         public void Ping()
         {
-            Dictionary<string, string> pingDetails = new OtherDataLib().PingDbDetails();
+            Dictionary<string, string> pingDetails = new DevTestDataLib().PingDbDetails();
 
             Assert.IsTrue(pingDetails.ContainsKey("DB Server"));
             Assert.IsTrue(!string.IsNullOrEmpty(pingDetails["DB Server"]));
