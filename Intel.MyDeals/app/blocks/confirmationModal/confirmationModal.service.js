792dc8f61b0d92c22c4663b70b13adc46a8fe742
Biradar, Mahesh <mahesh.biradar@intel.com>
2017-01-11 10:47:56 +0000

    Dependency error fix for confirmationModal service
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1406753

--- Intel.MyDeals/app/blocks/confirmationModal/confirmationModal.service.js
+++ Intel.MyDeals/app/blocks/confirmationModal/confirmationModal.service.js
@@ -1,59 +1,62 @@
 ﻿(function () {
-	'use strict';
+    'use strict';
 
-	angular
+    angular
         .module('blocks.confirmationModal')
         .service('confirmationModal', confirmationModal);
 
-	confirmationModal.$inject = ['$uibModal'];
+    confirmationModal.$inject = ['$uibModal'];
 
-	function confirmationModal($uibModal) {
+    function confirmationModal($uibModal) {
+        var modalDefaults = {
+            backdrop: true,
+            keyboard: true,
+            modalFade: true,
+            templateUrl: '/app/blocks/confirmationModal/confirmationModal.html'
+        };
 
-		var modalDefaults = {
-			backdrop: true,
-			keyboard: true,
-			modalFade: true,
-			templateUrl: '/app/blocks/confirmationModal/confirmationModal.html'
-		};
+        var modalOptions = {
+            closeButtonText: 'Cancel',
+            actionButtonText: 'OK',
+            headerText: 'Proceed?',
+            bodyText: 'Perform this action?'
+        };
 
-		var modalOptions = {
-			closeButtonText: 'Cancel',
-			actionButtonText: 'OK',
-			headerText: 'Proceed?',
-			bodyText: 'Perform this action?'
-		};
+        this.showModal = function (customModalDefaults, customModalOptions) {
+            if (!customModalDefaults) customModalDefaults = {};
+            customModalDefaults.backdrop = 'static';
+            return this.show(customModalDefaults, customModalOptions);
+        };
 
-		this.showModal = function (customModalDefaults, customModalOptions) {
-			if (!customModalDefaults) customModalDefaults = {};
-			customModalDefaults.backdrop = 'static';
-			return this.show(customModalDefaults, customModalOptions);
-		};
+        this.show = function (customModalDefaults, customModalOptions) {
+            // Create temp objects to work with since we're in a singleton service
+            var tempModalDefaults = {};
+            var tempModalOptions = {};
 
-		this.show = function (customModalDefaults, customModalOptions) {
-			// Create temp objects to work with since we're in a singleton service
-			var tempModalDefaults = {};
-			var tempModalOptions = {};
+            // Map angular-ui modal custom defaults to modal defaults defined in service
+            angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
 
-			// Map angular-ui modal custom defaults to modal defaults defined in service
-			angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+            // Map modal.html $scope custom properties to defaults defined in service
+            angular.extend(tempModalOptions, modalOptions, customModalOptions);
 
-			// Map modal.html $scope custom properties to defaults defined in service
-			angular.extend(tempModalOptions, modalOptions, customModalOptions);
+            if (!tempModalDefaults.controller) {
+                tempModalDefaults.controller = modalController;
 
-			if (!tempModalDefaults.controller) {
-				tempModalDefaults.controller = function ($scope, $uibModalInstance) {
-					$scope.modalOptions = tempModalOptions;
-					$scope.modalOptions.ok = function (result) {
-						$uibModalInstance.close(result);
-					};
-					$scope.modalOptions.close = function (result) {
-						$uibModalInstance.dismiss('cancel');
-					};
-				};
-			}
+                // inject the dependency here
+                modalController.$inject = ['$scope', '$uibModalInstance'];
 
-			return $uibModal.open(tempModalDefaults).result;
-		};
+                function modalController($scope, $uibModalInstance) {
+                    $scope.modalOptions = tempModalOptions;
+                    $scope.modalOptions.ok = function (result) {
+                        $uibModalInstance.close(result);
+                    };
+                    $scope.modalOptions.close = function (result) {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+                };
+            }
 
-	}
+            return $uibModal.open(tempModalDefaults).result;
+        };
+    }
 }());
