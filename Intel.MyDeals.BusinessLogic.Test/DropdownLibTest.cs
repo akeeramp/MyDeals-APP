409c385bbecb1a87ab86a96ed90c020e842e0af4
jjyeh <jjyeh@amr.tfs.local>
2017-03-03 01:34:08 +0000

    admin dropdown unit tests
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1414937

---
+++ Intel.MyDeals.BusinessLogic.Test/DropdownLibTest.cs
@@ -0,0 +1,90 @@
+﻿using Intel.MyDeals.BusinessLogic;
+using Intel.MyDeals.DataLibrary.Test;
+using Intel.MyDeals.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using Intel.MyDeals.IDataLibrary;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Intel.MyDeals.BusinessLogic.Test
+{
+    [TestFixture]
+    public class DropdownLibTest
+    {
+		[TestFixtureSetUp]
+		public void SetupUserAndDatabase()
+		{
+			Console.WriteLine("Started Dropdown Library Tests.");
+			OpUserStack.EmulateUnitTester();
+			UnitTestHelpers.SetDbConnection();
+		}
+
+		[TestFixtureTearDown]
+		public void AfterTheCurrentTestFixture()
+		{
+			Console.WriteLine("Completed Dropdown Library Tests.");
+		}
+		
+		[TestCase]
+        public void DropdownsGetAll()
+		{
+			// Arrange, ACT
+			IEnumerable<Dropdown> results = new DropdownLib().GetDropdowns();
+			
+			// Assert
+			Assert.IsTrue(results.Any());
+		}
+
+        [TestCase]
+        public void DropdownsGetDealTypes()
+        {
+            // Arrange, ACT
+            IEnumerable<Dropdown> results = new DropdownLib().GetDealTypesDropdowns();
+
+            // Assert
+            Assert.IsTrue(results.Any());
+        }
+
+        [TestCase]
+        public void DropdownsGetGroups()
+        {
+            // Arrange, ACT
+            IEnumerable<Dropdown> results = new DropdownLib().GetDropdownGroups();
+
+            // Assert
+            Assert.IsTrue(results.Any());
+        }
+
+        [TestCase]
+        public void BasicDropdownsGetAll()
+        {
+            // Arrange, ACT
+            IEnumerable<BasicDropdown> results = new DropdownLib().GetBasicDropdowns();
+
+            // Assert
+            Assert.IsTrue(results.Any());
+        }
+
+        [TestCase] 
+		public void BasicDropdownsUpdate()
+		{
+            // ARRANGE
+            BasicDropdown dd = new BasicDropdown();
+            dd.OBJ_SET_TYPE_SID = 1; //hardcoded "All Deals" id
+            dd.ATRB_SID = 18; //hardcoded "Program ECAP Type" id
+            dd.DROP_DOWN = "TEST";
+            dd.ACTV_IND = false;
+
+            // ACT
+            BasicDropdown results = new DropdownLib().ManageBasicDropdowns(dd, CrudModes.Insert);
+
+			// ASSERT
+			Assert.IsTrue(results.ATRB_LKUP_SID > -1);
+        }
+
+        //TODO: unit tests for insert/delete and more specific update cases
+		
+    }
+}
---
+++ Intel.MyDeals.DataLibrary.Test/DropdownDataLibTest.cs
@@ -0,0 +1,43 @@
+﻿using Intel.MyDeals.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using System;
+
+namespace Intel.MyDeals.DataLibrary.Test
+{
+    [TestFixture]
+    public class DropdownDataLibTest
+	{
+		[TestFixtureSetUp]
+		public void SetupUserAndDatabase()
+		{
+			Console.WriteLine("Started Dropdown Data Library tests");
+			OpUserStack.EmulateUnitTester();
+			UnitTestHelpers.SetDbConnection();
+		}		
+		
+		[TestCase]
+		public void DropdownGetAll()
+		{
+            // ARRANGE / ACT
+            List<Dropdown> results = new DropdownDataLib().GetDropdowns();
+
+			// ASSERT
+			Assert.IsTrue(results.Any());
+		}
+
+        [TestCase]
+        public void BasicDropdownGetAll()
+        {
+            // ARRANGE / ACT
+            List<BasicDropdown> results = new DropdownDataLib().GetBasicDropdowns();
+
+            // ASSERT
+            Assert.IsTrue(results.Any());
+        }
+
+        //TODO: unit tests for insert/update/delete of basic dropdown
+
+	}
+}
