6dc768b06ff821cffa80c97da903658ce340c436
Biradar, Mahesh <mahesh.biradar@intel.com>
2017-04-28 06:22:52 +0000

    Added startsWith function in util.js to support IE
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1424678

--- Intel.MyDeals/app/blocks/util.js
+++ Intel.MyDeals/app/blocks/util.js
@@ -1,31 +1,37 @@
-﻿/* 
+﻿/*
  * General common javascript extensions that can be utilized in all js files
  */
 
-
 // Check if an array contains the parameter
 Array.prototype.contains = function (obj) {
-	var i = this.length;
-	while (i--) {
-		if (this[i] === obj) {
-			return true;
-		}
-	}
-	return false;
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
 }
 
-// check if an element exists in array using a comparer function 
+// check if an element exists in array using a comparer function
 // comparer : function(currentElement)
 Array.prototype.inArray = function (comparer) {
-	for (var i = 0; i < this.length; i++) {
-		if (comparer(this[i])) return true;
-	}
-	return false;
+    for (var i = 0; i < this.length; i++) {
+        if (comparer(this[i])) return true;
+    }
+    return false;
 };
 
 // adds an element to the array if it does not already exist using a comparer function
 Array.prototype.pushIfNotExist = function (element, comparer) {
-	if (!this.inArray(comparer)) {
-		this.push(element);
-	}
+    if (!this.inArray(comparer)) {
+        this.push(element);
+    }
 };
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+    };
+}
