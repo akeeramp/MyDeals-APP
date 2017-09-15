eb457d956e56a31eeda2a6d70dfa51958d6cd88f
Eneboe, Tory J <tory.j.eneboe@intel.com>
2017-09-15 21:00:30 +0000

    Added todo comment.
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1445477

--- Intel.MyDeals.BusinessLogic/UserPreferences/UserPreferencesLib.cs
+++ Intel.MyDeals.BusinessLogic/UserPreferences/UserPreferencesLib.cs
@@ -27,6 +27,7 @@ namespace Intel.MyDeals.BusinessLogic
         /// <returns>list of UserPreferences</returns>
         public List<UserPreferences> GetUserPreferences(string category, string subCategory)
         {
+            // TODO::TJE
             // Commented out until DB gets it's parts in place for this to work.
             //return _userPreferencesDataLib.GetUserPreferences(category, subCategory).ToList();
             return null;
