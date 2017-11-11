61cf12c30f7f3e5fe16a8ec6d70e0aca42c2dc61
Tipping, Michael H <michael.h.tipping@intel.com>
2017-11-11 01:08:43 +0000

    DE32497 - Fix for Legal user in PCT screen
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1453657

--- Intel.MyDeals/app/contract/controllers/managerPct.controller.js
+++ Intel.MyDeals/app/contract/controllers/managerPct.controller.js
@@ -27,7 +27,6 @@ function managerPctController($scope, $state, objsetService, logger, $timeout, d
 
     var hasNoPermission = !$scope.root.CAN_EDIT_COST_TEST;
     var hasNoPermissionOvr = !$scope.root.CAN_EDIT_COST_TEST && window.usrRole !== "Legal";
-    var hasNoPermissionNotLegal = hasNoPermission || window.usrRole !== "Legal";
     var hasPermissionPrice = window.usrRole === "DA" || window.usrRole === "Legal" || ((window.usrRole === "GA" || window.usrRole === "SA") && window.isSuper);
     
     $timeout(function () {
@@ -433,7 +432,7 @@ function managerPctController($scope, $state, objsetService, logger, $timeout, d
             title: "Max RPU",
             format: "{0:c}",
             width: "100px",
-            hidden: hasNoPermissionNotLegal,
+            hidden: !hasPermissionPrice,
             parent: false
         },
         "ECAP_PRC": {
