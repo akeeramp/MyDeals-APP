05b07377fda393f0bc1c0d7b6de3d29691da1d6c
Narayanan, Valliappan <valliappan.narayanan@intel.com>
2018-01-17 21:28:10 +0000

    Remerged Quote letter changes
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1462200

--- Intel.MyDeals/Controllers/API/QuoteLetterController.cs
+++ Intel.MyDeals/Controllers/API/QuoteLetterController.cs
@@ -1,7 +1,14 @@
-﻿using Intel.MyDeals.Entities;
-using Intel.MyDeals.IBusinessLogic;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
+using Intel.MyDeals.App;
+using Intel.MyDeals.Entities;
+using Intel.MyDeals.IBusinessLogic;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Net.Http.Headers;
+using System;
 
 namespace Intel.MyDeals.Controllers.API
 {
@@ -16,6 +23,43 @@ namespace Intel.MyDeals.Controllers.API
             this._quoteLetterLib = quoteLetterLib;
         }
 
+        [HttpGet]
+        [Route("GetDealQuoteLetter/{dealId}")]
+        public HttpResponseMessage GetDealQuoteLetter(string dealId)
+        {
+            byte[] quoteLetterFinalBytes = null;
+            var result = new HttpResponseMessage(HttpStatusCode.OK);
+
+            var quoteLetterFile = SafeExecutor(() => _quoteLetterLib.GetDealQuoteLetter(dealId)
+                , $"Unable to download quote letter for {dealId}"
+            );
+            quoteLetterFinalBytes = quoteLetterFile.Content;
+            
+            if (quoteLetterFinalBytes != null)
+            {
+                Stream stream = new MemoryStream(quoteLetterFinalBytes);
+                result.Content = new StreamContent(stream);
+            }
+
+            string fName = quoteLetterFile.Name;
+
+            if (!string.IsNullOrEmpty(fName))
+            {
+                string[] swapString = { "(", ")", "-", "&", "'", "*", "^", " " };
+                foreach (string s in swapString)
+                {
+                    fName = fName.Replace(s, "_");
+                }
+                result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                result.Content.Headers.Add("Content-Disposition", String.Format("attachment;filename={0}", fName));
+            }
+            else
+                result = Request.CreateResponse(HttpStatusCode.NotFound);
+
+            return result;
+        }
+
         [Authorize]
         [HttpGet]
         [Route("GetQuoteLetterTemplates")]
@@ -26,6 +70,5 @@ namespace Intel.MyDeals.Controllers.API
                 , $"Unable to get Quote Letter Templates"
             );
         }
-
     }
 }
