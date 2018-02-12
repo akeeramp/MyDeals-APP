d45b6856eadc45d18b5f626e96c9df8a1fd45734
Kundu, Saurav <saurav.kundu@intel.com>
2018-02-12 06:00:15 +0000

    After removing Timezone from the Time it was unable to convert time to particular format in Timeline
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1465726

--- Intel.MyDeals/app/core/directives/timeline/timelineDetails.directive.js
+++ Intel.MyDeals/app/core/directives/timeline/timelineDetails.directive.js
@@ -25,24 +25,12 @@ function timelineDetails($compile, dataService, $timeout, logger, $linq) {
                 $scope.loading = true;
                 dataService.get("api/Timeline/GetTimelineDetails/" + $scope.opId + "/" + $scope.opObjType).then(function (response) {
                     $scope.loading = false;
+                    response.data.forEach(function (obj) {
+                        obj.HIST_EFF_FR_DTM = new Date(obj.HIST_EFF_FR_DTM);
+                    });
                     $scope.timelineData = response.data;
                     $scope.selectedObj = response.data;
                     $scope.isFilterApplied = false;
-                    $scope.approverList = $linq.Enumerable().From(response.data)
-                        .GroupBy(function (x) {
-                            return (x.CHG_EMP_WWID);
-                        }).Select(function (x) {
-                            return {
-                                'CHG_EMP_WWID': x.source[0].CHG_EMP_WWID,
-                                'NAME': x.source[0].FRST_NM + ' ' + x.source[0].LST_NM
-                            };
-                        })
-                        .ToArray();
-                    $scope.approverList = $scope.approverList.filter(function (value) {
-                        return value.NAME.trim().length > 0;
-                    });
-
-
                     return response.data;
                 },
                     function (response) {
@@ -59,67 +47,10 @@ function timelineDetails($compile, dataService, $timeout, logger, $linq) {
                     $scope.btnText = 'Show more ';
                     $scope.numberOfRecrods = 5;
                 }                    
-                
-            }
+               
+            }          
 
-            $scope.approverChange = function (data) {
-                $scope.selectedObj = $scope.timelineData;
-                if (data) {
-                    $scope.selectedApprover = data.CHG_EMP_WWID;
-                }
-                else {
-                    $scope.selectedApprover = null;
-                }
-                $scope.approverFilter(); 
-            };
-
-            $scope.changeStartDate = function (data) {                
-                $scope.selectedObj = $scope.timelineData;
-                $scope.startDate = moment(data).format("l");
-                $scope.approverFilter(); 
-            };
-            $scope.changeEndDate = function (data) {                
-                $scope.selectedObj = $scope.timelineData;
-                $scope.endDate = moment(data).format("l");
-                $scope.approverFilter(); 
-            };
-            $scope.approverFilter = function () {
-                $scope.isFilterApplied = false;
-                if ($scope.selectedApprover) {
-                    $scope.isFilterApplied = true;
-                    $scope.selectedObj = $scope.selectedObj.filter(function (item) {
-                        return item.CHG_EMP_WWID == $scope.selectedApprover;
-                    });
-                }                
-
-                if ($scope.startDate) {
-                    $scope.isFilterApplied = true;
-                    $scope.selectedObj = $scope.selectedObj.filter(function (item) {
-                        return Date.parse(moment(item.HIST_EFF_FR_DTM).format("l")) >= Date.parse($scope.startDate);
-                    });
-                }
-
-                if ($scope.endDate) {
-                    $scope.isFilterApplied = true;
-                    $scope.selectedObj = $scope.selectedObj.filter(function (item) {
-                        return Date.parse(moment(item.HIST_EFF_FR_DTM).format("l")) <= Date.parse($scope.endDate);
-                    });
-                }                
-            };
-
-            $scope.removeFilter = function () {
-                $scope.selectedObj = $scope.timelineData;
-                $scope.isFilterApplied = false;
-                $scope.selectedApprover = null;
-                $scope.startDate = null;
-                $scope.endDate = null;
-
-                $("#approverList").data("kendoDropDownList").select(-1);
-                $("#startDatePicker").data("kendoDatePicker").value(null);
-                $("#endDatePicker").data("kendoDatePicker").value(null);
-                
-
-            };
+            
         }],
         link: function (scope, element, attrs) {
             
