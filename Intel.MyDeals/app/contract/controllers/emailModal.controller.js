8e9febf2a9a735e06d4ac6e19ad4811d717e4e42
pweckenr <pweckenr@amr.tfs.local>
2018-03-08 18:29:15 +0000

    US98221 - user email displays the chinese name
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1469602

--- Intel.MyDeals/app/contract/controllers/emailModal.controller.js
+++ Intel.MyDeals/app/contract/controllers/emailModal.controller.js
@@ -45,25 +45,35 @@ function emailModalCtrl($scope, $uibModalInstance, dataItem) {
                 dataType: "json"
             }
         },
+        schema: {
+            parse: function(data) {
+                // Example adding a new field to the received data
+                // that computes price as price times quantity.
+                $.each(data, function(idx, elem) {
+                    elem.searchText = elem.LST_NM + ", " + elem.FRST_NM;
+                });
+                return data;
+            }
+        },
         filter: $scope.getFilter()
     });
 
     $scope.selectOptions = {
         placeholder: "Select email address...",
-        dataTextField: "EMAIL_ADDR",
-        dataValueField: "EMAIL_ADDR",
+        dataTextField: "searchText",
+        dataValueField: "searchText",
         itemTemplate: '<div class="tmpltItem">' +
                         '<div class="fl tmpltIcn"><i class="intelicon-email-message-solid"></i></div>' +
                         '<div class="fl tmpltContract"><div class="tmpltPrimary">#: data.LST_NM #, #: data.FRST_NM #</div><div class="tmpltSecondary">#: data.EMAIL_ADDR #</div></div>' +
                         '<div class="fr tmpltRole">#: data.ROLE_NM #</div>' +
                         '<div class="clearboth"></div>' +
                         '</div>',
-        valuePrimitive: true,
+        valuePrimitive: false,
         filter: "contains",
         autoBind: false,
         dataSource: $scope.ds
     };
-    $scope.selectedIds = [4, 7];
+    $scope.selectedIds = [];
 
     $scope.editorOptions = {
         tools: [
@@ -86,16 +96,19 @@ function emailModalCtrl($scope, $uibModalInstance, dataItem) {
     }
 
     $scope.disableEmailButton = function () {
-        return ($scope.dataItem.to.length === 0 || $scope.dataItem.subject === "" || $scope.dataItem.body === "")
+        return ($scope.dataItem.to.length === 0 || $scope.dataItem.subject === "" || $scope.dataItem.body === "");
     }
 
     $scope.sendEmail = function() {
         var url = "/Email/EmailNotification";
+        var to = [];
+        for (var e = 0; e < $scope.dataItem.to.length; e++) to.push($scope.dataItem.to[e].EMAIL_ADDR);
+
         var dataItem = {
             Subject: $scope.dataItem.subject,
             Body: $scope.dataItem.body,
             From: $scope.dataItem.from,
-            To: $scope.dataItem.to
+            To: to
         };
         op.ajaxPostAsync(url, dataItem, function(e) {
         }, function (e) {
