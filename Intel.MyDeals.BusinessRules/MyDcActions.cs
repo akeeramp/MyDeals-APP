9409a6b6c404e28ba793b9fbb64c868991a15759
pweckenr <pweckenr@amr.tfs.local>
2018-03-09 01:53:12 +0000

    DE36197 - Deal Editor: Sold to ID error incorrect
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1469692

--- Intel.MyDeals.BusinessRules/MyDcActions.cs
+++ Intel.MyDeals.BusinessRules/MyDcActions.cs
@@ -691,8 +691,8 @@ namespace Intel.MyDeals.BusinessRules
             if (progPayment == "Backend" || !string.IsNullOrEmpty(sold)) return;
 
             int custId = int.Parse(r.Dc.GetDataElementValue(AttributeCodes.CUST_MBR_SID));
-            string prcCd = DataCollections.GetCustomerDivisions().Where(c => c.CUST_NM_SID == custId && c.ACTV_IND).Select(c => c.PRC_GRP_CD).FirstOrDefault();
-            if (string.IsNullOrEmpty(prcCd) && !de.IsReadOnly)
+            string prcCd = DataCollections.GetCustomerDivisions().Where(c => (c.CUST_NM_SID == custId || c.CUST_DIV_NM_SID == custId) && c.ACTV_IND).Select(c => c.PRC_GRP_CD).FirstOrDefault();
+            if (string.IsNullOrEmpty(prcCd)) // && !de.IsReadOnly
             {
                 de.AddMessage("Frontend Deals cannot be created if no sold to values are selected and the customer doesn't have a Price Group Code.");
             }
