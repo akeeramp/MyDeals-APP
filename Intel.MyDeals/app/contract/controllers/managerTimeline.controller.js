3d375278310283526622164af431451476cbb20f
pweckenr <pweckenr@amr.tfs.local>
2018-03-22 05:09:02 +0000

    US99612 - Contract History
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1472064

--- Intel.MyDeals/app/contract/controllers/managerTimeline.controller.js
+++ Intel.MyDeals/app/contract/controllers/managerTimeline.controller.js
@@ -9,9 +9,9 @@
 
     SetRequestVerificationToken.$inject = ['$http'];
 
-    managerTimelineController.$inject = ['$scope', '$timeout'];
+    managerTimelineController.$inject = ['$scope', '$timeout', '$window'];
 
-    function managerTimelineController($scope, $timeout) {
+    function managerTimelineController($scope, $timeout, $window) {
 
         kendo.culture("en-US");
 
@@ -40,6 +40,49 @@
                 }, 500);
             }
         });
+
+        $scope.gridOptions = {
+            dataSource: $scope.timelineDs,
+            sortable: true,
+            resizable: true,
+            pageable: true,
+            filterable: true,
+            scrollable: true,
+            columns: [{
+                field: "OBJ_DESC",
+                title: "Object Type",
+                width: "140px"
+            }, {
+                field: "OBJ_SID",
+                title: "Object Id",
+                width: "120px"
+            }, {
+                field: "FLAG",
+                title: "Flag",
+                width: "90px",
+                filterable: { multi: true, search: true }
+            }, {
+                field: "ATRB_DESC",
+                title: "Action Source",
+                width: "140px",
+                filterable: { multi: true, search: true }
+            }, {
+                field: "ATRB_VAL",
+                title: "Action Value",
+                width: "200px"
+            }, {
+                field: "FRST_NM",
+                title: "Changed By",
+                template: "#= FRST_NM # #= LST_NM #",
+                width: "200px",
+                filterable: { multi: true, search: true }
+            }, {
+                field: "HIST_EFF_FR_DTM",
+                title: "Date Changed",
+                width: "160px"
+            }]
+
+        }
 
         $scope.timelineDs.read();
 
