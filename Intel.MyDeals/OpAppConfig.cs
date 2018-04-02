48297a00a32e78ed2ec20843f1ae0a67eab012fa
Biradar, Mahesh <mahesh.biradar@intel.com>
2018-04-02 05:18:13 +0000

    HTTPS changes for DB logs
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1473749

--- Intel.MyDeals/OpAppConfig.cs
+++ Intel.MyDeals/OpAppConfig.cs
@@ -4,71 +4,74 @@ using Intel.MyDeals.BusinessLogic;
 using Intel.MyDeals.Entities;
 using Intel.MyDeals.Entities.Logging;
 using Intel.Opaque;
- 
+
 namespace Intel.MyDeals
 {
     public static class OpAppConfig
     {
-		static OpAppConfig()
-		{
-			try
-			{
-				// Logging 
-				OpLog.LogConfig = new LoggingLib().GetLogConfig();
-				OpLogPerf.Enabled = OpLog.LogConfig.IsActive;
-
-				// Entities' web API
-				MyDealsWebApiUrl.ROOT_URL = OpCurrentConfig.CurrentURL;
-				// TODO josiTODO: remove after localhost test is done
-				if (MyDealsWebApiUrl.ROOT_URL == "localhost")
-				{
-					MyDealsWebApiUrl.ROOT_URL += ":55490";
-				}
-				MyDealsWebApiUrl.ROOT_URL = "http://" + MyDealsWebApiUrl.ROOT_URL;
-			}
-			catch (Exception ex)
-			{
-				OpLogPerf.Log(ex);
-				throw;
-			}
-		}
-
-		public static OpCore Init()
+        static OpAppConfig()
         {
             try
-			{
-				// Check if OpCore has already been created
-				if (OpCore.Instance != null) return OpCore.Instance;
+            {
+                // Logging
+                OpLog.LogConfig = new LoggingLib().GetLogConfig();
+                OpLogPerf.Enabled = OpLog.LogConfig.IsActive;
+
+                // Entities' web API
+                MyDealsWebApiUrl.ROOT_URL = OpCurrentConfig.CurrentURL;
+                // TODO josiTODO: remove after localhost test is done
+                if (MyDealsWebApiUrl.ROOT_URL == "localhost")
+                {
+                    MyDealsWebApiUrl.ROOT_URL = "http://" + MyDealsWebApiUrl.ROOT_URL + ":55490";
+                }
+                else
+                {
+                    MyDealsWebApiUrl.ROOT_URL = "https://" + MyDealsWebApiUrl.ROOT_URL;
+                }
+            }
+            catch (Exception ex)
+            {
+                OpLogPerf.Log(ex);
+                throw;
+            }
+        }
+
+        public static OpCore Init()
+        {
+            try
+            {
+                // Check if OpCore has already been created
+                if (OpCore.Instance != null) return OpCore.Instance;
 
                 // Instantiate an Instance
                 OpCore opCore = OpCore.Instance = new OpCore(false);
-				
-				// Since we are NOT using OPAQUE for environment switching, we need to manually set the apptoken
-				opCore.AppToken = new OpAppToken
+
+                // Since we are NOT using OPAQUE for environment switching, we need to manually set the apptoken
+                opCore.AppToken = new OpAppToken
                 {
                     AppCd = "MyDeals",
                     OpEnvironment = new OpEnvironment
                     {
-                        App = new OpApp {AppName = "MyDeals"},
-                        EnvLoc = new OpEnvLocation {Location = AppLib.GetEnvironment()}
+                        App = new OpApp { AppName = "MyDeals" },
+                        EnvLoc = new OpEnvLocation { Location = AppLib.GetEnvironment() }
                     },
                 };
                 OpLog.OpAppToken = opCore.AppToken;
-			
-				// Congfig Authentication
-				opCore.Authentication = new OpAuthentication
+
+                // Congfig Authentication
+                opCore.Authentication = new OpAuthentication
                 {
                     Enabled = true,
                     OpAuthenticationType = OpAuthenticationType.Negotiate,
                     AppCustomAuthenticationFunc = CustomAuthentication
                 };
 
-				return OpCore.Instance;
+                return OpCore.Instance;
             }
             catch (Exception ex)
             {
-				OpLogPerf.Log(ex);
-				throw;
+                OpLogPerf.Log(ex);
+                throw;
             }
         }
 
@@ -76,14 +79,11 @@ namespace Intel.MyDeals
         {
             // Loading user token from IDSID, to test out non-existant user, just append something to below ({Idsid = "A" + idsod}};)
             idsid = idsid.Trim();
-            OpUserToken opUserToken = new OpUserToken {Usr = {Idsid = idsid}};
+            OpUserToken opUserToken = new OpUserToken { Usr = { Idsid = idsid } };
 
             AppLib.PopulateUserSettings(opUserToken);
 
             return opUserToken.Usr.WWID == 0 ? null : AppLib.UserSettings[idsid.ToUpper()].UserToken;
         }
-
     }
-
-
 }
