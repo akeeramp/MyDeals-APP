e0a857133f67c9cd8565891386a1a8e0ab70e7fc
Tipping, Michael H <michael.h.tipping@intel.com>
2018-04-05 23:32:44 +0000

    DE37593 - Fix customer selection screen filters and all
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1474613

--- Intel.MyDeals/app/admin/employee/admin.manageEmployee.controller.js
+++ Intel.MyDeals/app/admin/employee/admin.manageEmployee.controller.js
@@ -49,6 +49,7 @@
                                 {
                                     response.data[c].USR_CUST = "[Please Add Customers]";
                                 }
+                                response.data[c].FULL_NAME = response.data[c].LST_NM + ", " + response.data[c].FRST_NM + " " + response.data[c].MI
                             }
                             e.success(response.data);
                             $scope.isDropdownsLoaded = true;
@@ -62,10 +63,11 @@
                 model: {
                     id: "EMP_WWID",
                     fields: {
-                        EMP_WWID: { type: "number" },
+                        EMP_WWID: { type: "string" }, // Changed from number to string so that we can remove the JS number formatting of inserting commas
                         FRST_NM: { type: "string" },
                         MI: { type: "string" },
                         LST_NM: { type: "string" },
+                        FULL_NAME: { type: "string" },
                         ACTV_IND: { type: "boolean" },
                         USR_ROLE: { type: "string" },
                         USR_GEOS: { type: "string" },
@@ -84,7 +86,7 @@
 
         $scope.gridOptions = {
             dataSource: $scope.dataSource,
-            filterable: gridConstants.filterable,
+            filterable: true, //gridConstants.filterable,
             sortable: true,
             selectable: true,
             resizable: true,
@@ -92,37 +94,146 @@
                 refresh: true,
                 pageSizes: gridConstants.pageSizes
             },
+            filter: function (e) {
+                if (e.field == "USR_ROLE" || e.field == "USR_GEOS") {
+                    if (e.filter !== undefined && e.filter !== null) { // Safety check for clearing out set.
+                        e.filter.filters.forEach(function (f) {
+                            f.operator = "contains";
+                        })
+                    }
+                }
+            },
+            // This only works in Kendo UI 2017 R2, we are on 2017 R1
+            filterMenuOpen: function (e) {
+                //if (e.field == "USR_ROLE" || e.field == "USR_GEOS") { // Snazy code to remove the clear button from columns not needed due to safety check above.
+                //    e.container.find("button[type='reset']").hide();
+                //    e.container.find("button[type='submit']").css("width", "100%");
+                //}
+                if (e.sender.dataSource.filter()) {
+                    e.sender.dataSource.filter().filters.forEach(function (f) {
+                        if (f.filters === undefined) // Single-instance - no sub filters defined
+                        {
+                            if (f.field == "USR_ROLE" || f.field == "USR_GEOS") {
+                                var checkbox = e.container.find("input[value='" + f.value + "']");
+                                if (checkbox[0] !== undefined && !checkbox[0].checked) {
+                                    e.container.find("input[value='" + f.value + "']").click()
+                                }
+                            }
+                        }
+                        else // Multi-instance - go through each instance
+                        { 
+                            if (f.field == "USR_ROLE" || f.field == "USR_GEOS") { // Safety code in case there is a single isolated element hanging off the object - likely not needed
+                                var checkbox = e.container.find("input[value='" + f.value + "']");
+                                if (checkbox[0] !== undefined && !checkbox[0].checked) {
+                                    e.container.find("input[value='" + f.value + "']").click()
+                                }
+                            }
+                            else // Branch taken if there is an embedded filter set..
+                            {
+                                f.filters.forEach(function (g) {
+                                    if (g.field == "USR_ROLE" || g.field == "USR_GEOS") {
+                                        var checkbox = e.container.find("input[value='" + g.value + "']");
+                                        if (checkbox[0] !== undefined && !checkbox[0].checked) {
+                                            e.container.find("input[value='" + g.value + "']").click()
+                                        }
+                                    }
+                                })
+                            }
+
+                        }
+
+                    })
+                }
+            },
             columns: [
             {
                 field: "EMP_WWID",
                 title: "WWID",
+                filterable: {
+                    extra: false,
+                    operators: {
+                        string: {
+                            eq: "Is equal to",
+                            contains: "Contains"
+                        }
+                    }
+                },
                 width: "100px"
             },
             {
-                field: "LST_NM",
+                field: "FULL_NAME",
                 title: "Name",
-                width: "100px",
-                template: "#=LST_NM#, #=FRST_NM# #=MI#"
+                filterable: {
+                    extra: false,
+                    operators: {
+                        string: {
+                            contains: "Contains"
+                        }
+                    }
+                },
+                width: "100px"
+                //template: "#=LST_NM#, #=FRST_NM# #=MI#" // Replaced by FULL_NAME
             },
             {
                 field: "USR_ROLE",
                 title: "Roles",
+                filterable: {
+                    multi: true,
+                    dataSource: [
+                        { USR_ROLE: "CBA"} ,
+                        { USR_ROLE: "DA"} ,
+                        { USR_ROLE: "FSE"} ,
+                        { USR_ROLE: "Finance"} ,
+                        { USR_ROLE: "GA"} ,
+                        { USR_ROLE: "Legal"} ,
+                        { USR_ROLE: "MyDeals SA"} ,
+                        { USR_ROLE: "Net ASP SA"} ,
+                        { USR_ROLE: "RA"} ,
+                        { USR_ROLE: "Rebate Forecast SA"} ,
+                        { USR_ROLE: "WRAP SA"}]
+                },
                 width: "100px",
             },
             {
                 field: "USR_GEOS",
                 title: "Geos",
+                filterable: {
+                    multi: true,
+                    dataSource: [
+                        { USR_GEOS: "APAC"} ,
+                        { USR_GEOS: "ASMO"} ,
+                        { USR_GEOS: "EMEA"} ,
+                        { USR_GEOS: "IJKK"} ,
+                        { USR_GEOS: "PRC"} ,
+                        { USR_GEOS: "Worldwide"}]
+                },
                 width: "100px",
             },
             {
                 field: "USR_CUST",
                 title: "Customers",
+                filterable: {
+                    extra: false,
+                    operators: {
+                        string: {
+                            contains: "Contains"
+                        }
+                    }
+                },
                 template: "#=gridUtils.customersFormatting(data, 'USR_CUST', 'USR_ROLE', 'USR_GEOS')#",
                 width: "250px",
             },
             {
                 field: "USR_VERTS",
                 title: "Verticals",
+                filterable: {
+                    extra: false,
+                    operators: {
+                        string: {
+                            contains: "Contains"
+                        }
+                    }
+                },
                 width: "100px",
             },
             {
@@ -130,7 +241,6 @@
                 title: "Active",
                 template: "#=gridUtils.booleanDisplay(data, 'ACTV_IND')#",
                 attributes: { style: "text-align: center;" },
-                filterable: false,
                 width: "100px",
             },
             {
@@ -138,7 +248,6 @@
                 title: "Super",
                 template: "#=gridUtils.booleanDisplay(data, 'IS_SUPER')#",
                 attributes: { style: "text-align: center;" },
-                filterable: false,
                 width: "100px",
             },
             {
@@ -146,7 +255,6 @@
                 title: "Developer",
                 template: "#=gridUtils.booleanDisplay(data, 'IS_DEVELOPER')#",
                 attributes: { style: "text-align: center;" },
-                filterable: false,
                 width: "100px",
             },
             {
@@ -154,7 +262,6 @@
                 title: "Tester",
                 template: "#=gridUtils.booleanDisplay(data, 'IS_TESTER')#",
                 attributes: { style: "text-align: center;" },
-                filterable: false,
                 width: "100px",
             },
             {
