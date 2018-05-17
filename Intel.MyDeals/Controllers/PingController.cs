7a16427cddfa7e1417a175900fa45a26e9c2b7cb
pweckenr <pweckenr@amr.tfs.local>
2018-05-17 20:05:27 +0000

    error 500 + ping
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1480674

---
+++ Intel.MyDeals/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Intel.MyDeals.DataLibrary;
+
+namespace Intel.MyDeals.Controllers
+{
+    public class PingController : Controller
+    {
+        // GET: Ping
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Db()
+        {
+            ViewBag.PingResults = new DevTestDataLib().PingDbDetails();
+            return new RedirectResult(Url.Action("Index"));
+        }
+    }
+}
---
+++ Intel.MyDeals/app/core/directives/ping/ping.directive.js
@@ -0,0 +1,52 @@
+﻿angular
+    .module('app.core')
+    .directive('ping', ping);
+
+ping.$inject = ['$timeout'];
+
+function ping($timeout) {
+    kendo.culture("en-US");
+
+    return {
+        scope: {},
+        restrict: 'AE',
+        transclude: true,
+        templateUrl: '/app/core/directives/ping/ping.directive.html',
+        controller: ['$scope', 'dataService', function ($scope, dataService) {
+            $scope.pingCycle = 3000;
+            $scope.pingValues = [];
+            $scope.pingTime = null;
+
+            $scope.getClassName = function () {
+                if ($scope.pingTime === undefined || $scope.pingTime === null) {
+                    return "none";
+                } else if ($scope.pingTime > 200) {
+                    return "low";
+                } else if ($scope.pingTime > 100) {
+                    return "low";
+                }
+                return "high";
+            }
+
+            $scope.pingHost = function() {
+                $scope.ping = new Date;
+                $.ajax({
+                    url: "/Ping",
+                    cache: false,
+                    success: function (output) {
+                        $scope.pingTime = new Date - $scope.ping;
+                        $scope.pingValues.push($scope.pingTime);
+                        if ($scope.pingValues.length > 10) $scope.pingValues.shift();
+                        $timeout(function () {
+                            $scope.pingHost();
+                        }, $scope.pingCycle);
+                    }
+                });
+            }
+
+            $scope.pingHost();
+        }],
+        link: function (scope, element, attrs) {
+        }
+    };
+}
