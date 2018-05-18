be720f014fbf3c344bb838251477a45b1e56f4a0
Eneboe, Tory J <tory.j.eneboe@intel.com>
2018-05-18 22:21:00 +0000

    debug 500
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1480862

--- Intel.MyDeals/Controllers/HelpController.cs
+++ Intel.MyDeals/Controllers/HelpController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Mvc;
 using Intel.MyDeals.App;
+using Intel.MyDeals.Entities;
 using Intel.Opaque;
 
 namespace Intel.MyDeals.Controllers
@@ -8,14 +9,10 @@ namespace Intel.MyDeals.Controllers
     {
         public ActionResult Index()
         {
-            return View("index", new ApplicationViewModel
-            {
-                AppName = "aaa",
-                AppDescShort = "bbb",
-                AppDesc = "ccc",
-                AppCopy = "ddd",
-                AppCopyShort = "eee"                
-            });
+            AppLib.InitAvm(OpAppConfig.Init(), AvmforceMode.User);
+            return RedirectToAction("Index", "Home");
+
+            //return View(AppLib.AVM);
         }
 	}
 }
