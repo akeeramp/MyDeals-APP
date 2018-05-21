acc2f5d0d1dfb9a27d492c911118861304f85ff0
Velraj, Ajay <ajay.velraj@intel.com>
2018-05-21 09:12:19 +0000

    Adding project Cache Recycle for US104367
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1480942

---
+++ Intel.MyDeals.Utilities/Intel.MyDeals.CacheRecycle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intel.MyDeals.CacheRecycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
