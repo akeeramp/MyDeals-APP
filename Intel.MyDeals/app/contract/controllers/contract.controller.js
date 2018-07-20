d9696340341bca7f309343614a9170b09b84c3b2
Tipping, Michael H <michael.h.tipping@intel.com>
2018-07-20 00:31:26 +0000

    DE8996 - the deal view is not visible for all roles except tester
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1487624

--- Intel.MyDeals/app/contract/controllers/contract.controller.js
+++ Intel.MyDeals/app/contract/controllers/contract.controller.js
@@ -82,7 +82,7 @@
         $scope.C_DEL_DEALS = (window.usrRole === "FSE" || window.usrRole === "GA");
         $scope.CAN_VIEW_EXPORT = true;
         $scope.CAN_VIEW_EXCLUDE_GROUPS = true;
-        $scope.CAN_VIEW_ALL_DEALS = (window.isDeveloper || window.isTester);
+        $scope.CAN_VIEW_ALL_DEALS = true;  //Removed for DE8996 - (window.isDeveloper || window.isTester);
         $scope.canDeleteAttachment = function (wfStage) {
             return securityService.chkDealRules('C_DELETE_ATTACHMENTS', window.usrRole, null, null, wfStage);
         }
