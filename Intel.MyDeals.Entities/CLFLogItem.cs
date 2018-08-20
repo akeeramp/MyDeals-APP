025898f0e14d9c34725edfe919329bdaedb054f6
Biradar, Mahesh <mahesh.biradar@intel.com>
2018-08-20 03:42:13 +0000

    CLF Integration
    git-tfs-id: [https://tfs-fm-04.devtools.intel.com/tfs/DefaultCollection_TFS04]$/RDMS/MyDeals/MyDeals_Release/UI/main;C1490431

---
+++ Intel.MyDeals.Entities/CLFLogItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+
+namespace Intel.MyDeals.Entities
+{
+    public class CLFLogItem
+    {
+        public string MachineAccount { get; set; }
+
+        public string LogType { get; set; }
+
+        public string LogCategory { get; set; }
+
+        public string StepName { get; set; }
+
+        public string StepDetail { get; set; }
+
+        public string DBName { get; set; }
+
+        public string SchemaName { get; set; }
+
+        public string ServerMachineName { get; set; }
+
+        public string ServerMachineIP { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string SessionId { get; set; }
+
+        public CustomLog Custom { get; set; }
+    }
+
+    public class CustomLog
+    {
+        internal int str_MyDeals_Step;
+
+        public string str_MyDeals_UserName { get; set; }
+
+        public int int_MyDeals_RecordCount { get; set; }
+
+        public int int_MyDeals_ThreadId { get; set; }
+
+        public string str_MyDeals_Environment { get; set; }
+
+        public DateTime str_MyDeals_LogDateTime { get; set; }
+
+        public string str_MyDeals_RunTime { get; set; }
+    }
+}
--- Intel.MyDeals.Entities/Logging/LoggingClient.cs
+++ Intel.MyDeals.Entities/Logging/LoggingClient.cs
@@ -4,6 +4,9 @@ using System.Reflection;
 using Intel.Opaque;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Intel.MyDeals.Entities.Logging
 {
@@ -11,6 +14,9 @@ namespace Intel.MyDeals.Entities.Logging
     {
         private static object LOCK_OBJECT = new object();
         private static LoggingClient _InstanceOptimal = null;
+        private static string _cLFUrl = string.Empty;
+        private static string _cLFToken = string.Empty;
+        private static string _env = string.Empty;
 
         private static LoggingClient Instance
         {
@@ -82,8 +88,39 @@ namespace Intel.MyDeals.Entities.Logging
             }
         }
 
+        public static void SetCLFLogs()
+        {
+            if (_cLFUrl == string.Empty || _cLFToken == string.Empty || _env == string.Empty)
+            {
+                Configuration rootWebConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/Intel.MyDeals");
+                _cLFUrl = rootWebConfig.AppSettings.Settings["CLFUrl"].Value;
+                _cLFToken = rootWebConfig.AppSettings.Settings["CLFToken"].Value;
+                _env = rootWebConfig.AppSettings.Settings["Environment"].Value;
+            }
+        }
+
         /// <summary>
-        ///
+        /// Construct CLF client
+        /// </summary>
+        private static HttpClient CLFClient
+        {
+            get
+            {
+                HttpClientHandler handler = new HttpClientHandler();
+                HttpClient client = new HttpClient(handler)
+                {
+                    BaseAddress = new Uri(_cLFUrl)
+                };
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _cLFToken);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                return client;
+            }
+        }
+
+        /// <summary>
+        /// Upload to CLF
         /// </summary>
         /// <param name="messages"></param>
         public void UploadLogPerfLogs(IEnumerable<DbLogPerfMessage> messages)
@@ -92,6 +129,66 @@ namespace Intel.MyDeals.Entities.Logging
             {
                 Messages = OpSerializeHelper.ToJsonString(messages, true),
             });
+
+            Task.Run(() => UploadToCLF(messages));
+        }
+
+        /// <summary>
+        ///Convert DbPerfLog to CLF
+        /// </summary>
+        /// <param name="messages"></param>
+        /// <returns></returns>
+        public static List<CLFLogItem> ConvertToCLFEntity(IEnumerable<DbLogPerfMessage> messages)
+        {
+            SetCLFLogs();
+            var clfLogItem = new List<CLFLogItem>();
+            var sessionId = Guid.NewGuid().ToString();
+            foreach (var message in messages)
+            {
+                var clf = new CLFLogItem();
+                clf.DBName = "MyDeals";
+                clf.StartTime = message.STRT_DTM;
+                clf.EndTime = message.END_DTM;
+                clf.LogCategory = message.MSG_SRC;
+                clf.LogType = message.ERR_MSG ? "ERROR" : "INFO";
+                clf.ServerMachineName = message.CLNT_MCHN_NM;
+                clf.ServerMachineIP = message.CLNT_MCHN_NM;
+                clf.MachineAccount = "rdmscdms";
+                clf.SessionId = sessionId;
+                clf.StepName = message.MTHD;
+                clf.SchemaName = string.Empty;
+                clf.StepDetail = message.MSG;
+                clf.Custom = new CustomLog
+                {
+                    str_MyDeals_Step = message.STEP,
+                    str_MyDeals_Environment = _env,
+                    int_MyDeals_RecordCount = message.REC_CNT,
+                    str_MyDeals_LogDateTime = DateTime.Now,
+                    int_MyDeals_ThreadId = message.THRD_ID,
+                    str_MyDeals_UserName = message.LGN_NM,
+                };
+                clfLogItem.Add(clf);
+            }
+            return clfLogItem;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="messages"></param>
+        public static void UploadToCLF(IEnumerable<DbLogPerfMessage> messages)
+        {
+            try
+            {
+                var clf = ConvertToCLFEntity(messages);
+
+                CLFClient.PostAsJsonAsync("/Logger/SaveLogs", clf);
+            }
+            catch (Exception ex)
+            {
+                //eat the exception if CLF is failing..
+                Debug.WriteLine("CLF error " + ex.Message);
+            }
         }
     }
 }
