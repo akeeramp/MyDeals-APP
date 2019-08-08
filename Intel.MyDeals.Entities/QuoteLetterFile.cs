a8c6b5a4bdc2998eb13f9b171386ac9777ffc3a6
mhtippin <michael.h.tipping@intel.com>
2019-08-07 22:02:33 -0700

    Test

--- Intel.MyDeals.Entities/QuoteLetterFile.cs
+++ Intel.MyDeals.Entities/QuoteLetterFile.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace Intel.MyDeals.Entities
 {
     public class QuoteLetterFile
-    {
+    { // test
         public Byte[] Content { set; get; }
         public string Name { set; get; }
     }
