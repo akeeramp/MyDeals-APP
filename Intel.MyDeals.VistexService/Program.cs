8bbdcbc998fa7163b03cb03600589ed5af6dfaf4
GanthiX Ganesh <ganthix.ganesh@intel.com>
2020-03-12 18:08:38 +0530

    Rollback data

--- Intel.MyDeals.VistexService/Program.cs
+++ Intel.MyDeals.VistexService/Program.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using Intel.MyDeals.Entities;
 using Vistex;
+using System.Collections.Generic;
 
 namespace Intel.MyDeals.VistexService
 {
@@ -12,54 +13,60 @@ namespace Intel.MyDeals.VistexService
         static int iMaximumAttempts = Convert.ToInt32(ConfigurationManager.AppSettings["MaximumAttempts"]);
         static void Main(string[] args)
         {
-            using (VistexAdminDataLib vistexAdminDataLib = new VistexAdminDataLib())
+            int iAttempts = 1;
+            Console.WriteLine("Please select retry option:\n1.Auto Retry\n2.Manual Retry");
+            string strRetryKey = Console.ReadKey().Key.ToString().Remove(0, 1);
+            bool isAutoRetry = strRetryKey == "1";
+            Console.WriteLine("\nPlease wait while requesting service..");
+            ResponseType responseType = ResponseType.None;
+            List<VistexLogs> lstTransId = new List<VistexLogs>();
+            do
             {
-                VistexProductVerticalOutBound lstTemp = vistexAdminDataLib.GetProductVerticalBody(210);
-                VistexProductVerticalOutBound lstTemp1 = vistexAdminDataLib.GetProductVerticalBody(212);
-            }
-            //int iAttempts = 1;            
-            //Console.WriteLine("Please select retry option:\n1.Auto Retry\n2.Manual Retry");
-            //string strRetryKey = Console.ReadKey().Key.ToString().Remove(0, 1);
-            //bool isAutoRetry = strRetryKey == "1";
-            //Console.WriteLine("\nPlease wait while requesting service..");
-            //ResponseType responseType = ResponseType.None;
-            //do
-            //{
-            //    if (iAttempts > 1)
-            //    {
-            //        if(!isAutoRetry)
-            //        {
-            //            Console.WriteLine("\nYou have selected manual retry. Please press any key for next try..");
-            //            Console.ReadKey();
-            //        }
-            //        responseType = ResponseType.None;
-            //    }
-            //    Console.WriteLine(string.Format("\n{0} to push below Batch IDs..", iAttempts > 1 ? "Retrying" : "Trying"));
-            //    using (VistexAdminDataLib vistexAdminDataLib = new VistexAdminDataLib())
-            //    {
-            //        var transIds = vistexAdminDataLib.GetVistexDealOutBoundData().Select(x => x.TransanctionId).Distinct();
-            //        if (transIds.Count() > 0)
-            //            Console.WriteLine(string.Join("\n", transIds));
-            //        else
-            //            Console.WriteLine("There is no outbound data to push..");
-            //    }
-            //    using (VistexHttpService vistexHttpService = new VistexHttpService())
-            //    {
-            //        responseType = vistexHttpService.GetVistexOutBoundData();
-            //    }
-            //    Console.WriteLine(VistexHttpService.GetResposnseMessage(responseType));
-            //    iAttempts++;
-            //} while (responseType != ResponseType.Success && iAttempts <= iMaximumAttempts);
+                if (iAttempts > 1)
+                {
+                    if (!isAutoRetry)
+                    {
+                        Console.WriteLine("\nYou have selected manual retry. Please press any key for next try..");
+                        Console.ReadKey();
+                    }
+                    responseType = ResponseType.None;
+                }
+                Console.WriteLine(string.Format("\n{0} to push below Batch IDs..", iAttempts > 1 ? "Retrying" : "Trying"));
+                using (VistexAdminDataLib vistexAdminDataLib = new VistexAdminDataLib())
+                {
+                    lstTransId.AddRange((from result in vistexAdminDataLib.GetVistexDealOutBoundData()
+                                         select new VistexLogs
+                                         {
+                                             DealId = result.DealId,
+                                             TransanctionId = result.TransanctionId
+                                         }).ToList());
+                    if (lstTransId.Count() > 0)
+                        Console.WriteLine(string.Join("\n", lstTransId.Select(x => x.TransanctionId).Distinct()));
+                    else
+                        Console.WriteLine("There is no outbound data to push..");
+                }
+                using (VistexHttpService vistexHttpService = new VistexHttpService())
+                {
+                    responseType = vistexHttpService.GetVistexOutBoundData();
+                }
+                Console.WriteLine(VistexHttpService.GetResposnseMessage(responseType));
+                iAttempts++;
+            } while (responseType != ResponseType.Success && iAttempts <= iMaximumAttempts);
 
-            //if (responseType != ResponseType.Success && iAttempts >= iMaximumAttempts)
-            //{
-            //    Console.WriteLine("\nSending alert mail..\nPlease enter Email Ids to receive alert mail:");
-            //    using (VistexAdminDataLib vistexAdminDataLib = new VistexAdminDataLib())
-            //    {
-            //        vistexAdminDataLib.SendFailureMessage(responseType,Console.ReadLine());
-            //    }
-            //    Console.WriteLine("Alert mail has been sent");
-            //}            
+            if (responseType != ResponseType.Success && iAttempts >= iMaximumAttempts)
+            {
+                using (VistexAdminDataLib vistexAdminDataLib = new VistexAdminDataLib())
+                {
+                    foreach (VistexLogs temp in lstTransId)
+                    {
+                        Console.WriteLine(string.Concat("Rolling back for transanction Id ", temp.TransanctionId, ", Deal Id: ", temp.DealId));
+                        vistexAdminDataLib.UpdateStatus(temp.TransanctionId.Value, VistexStage.PO_Error_Rollback, temp.DealId, "Failure in service call..");
+                    }
+                    Console.WriteLine("\nSending alert mail..\nPlease enter Email Ids to receive alert mail:");
+                    vistexAdminDataLib.SendFailureMessage(responseType, Console.ReadLine());
+                }
+                Console.WriteLine("Alert mail has been sent");
+            }
 
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
