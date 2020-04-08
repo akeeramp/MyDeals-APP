5423d39622efad9ac3c3095fba9fe6c2dc110181
Biradar, Mahesh <mahesh.biradar@intel.com>
2020-04-08 11:02:04 +0530

    Convert the Single object to array during JSON serialization

--- Intel.MyDeals.Entities/TenderTransferObjects.cs
+++ Intel.MyDeals.Entities/TenderTransferObjects.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 
 namespace Intel.MyDeals.Entities
@@ -86,8 +88,9 @@ namespace Intel.MyDeals.Entities
             public string TargetSystem { get; set; }
             public string Action { get; set; }
             public string BatchId { get; set; }
-            public List<DealResponse> DealResponses { get; set; }
 
+            [JsonConverter(typeof(SingleOrListConverter<DealResponse>))]
+            public List<DealResponse> DealResponses { get; set; }
             public class DealResponse
             {
                 public int DealId { get; set; }
@@ -95,6 +98,38 @@ namespace Intel.MyDeals.Entities
                 public string Status { get; set; }
                 public int AgreementId { get; set; }
                 public string DealType { get; set; }
+            }
+        }
+
+        /// <summary>
+        /// This method converts Single object to List during deserialization
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public class SingleOrListConverter<T> : JsonConverter
+        {
+            public override bool CanConvert(Type objectType)
+            {
+                return (objectType == typeof(List<T>));
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                JToken token = JToken.Load(reader);
+                if (token.Type == JTokenType.Array)
+                {
+                    return token.ToObject<List<T>>();
+                }
+                return new List<T> { token.ToObject<T>() };
+            }
+
+            public override bool CanWrite
+            {
+                get { return false; }
+            }
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                throw new NotImplementedException();
             }
         }
     }
