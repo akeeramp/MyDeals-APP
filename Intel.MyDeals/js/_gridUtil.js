f76bb4cb93d1bf99cd913adde27ed29fea70ea8a
Michael H Tipping <michael.h.tipping@intel.com>
2021-03-09 09:23:11 -0700

    DE110174 - IQR: Fix Error message for Billing Start Date Validation to Account for Long Qrt
    DE110174 - IQR: Fix Error message for Billing Start Date Validation to Account for Long Qrt

--- Intel.MyDeals/js/_gridUtil.js
+++ Intel.MyDeals/js/_gridUtil.js
@@ -1032,9 +1032,9 @@ gridUtils.uiValidationErrorDetail = function (passedData) {
     var values = Object.values(passedData._behaviors.validMsg);
     var formattedMessage = '';
     values.forEach((msg) => {
-        formattedMessage += msg;
+        formattedMessage += msg.replace(/'/g, "");
     });
-    var tmplt = "<div class='uiControlDiv isReadOnlyCell'><div class='vert-center'><i class='valid-icon validf_{{ dataItem.PASSED_VALIDATION }} {{ (dataItem.PASSED_VALIDATION === undefined || dataItem.PASSED_VALIDATION === \"\") ? \"intelicon-protection-solid\" : (dataItem.PASSED_VALIDATION == \"Complete\") ? \"intelicon-protection-checked-verified-solid\" : \"intelicon-alert-solid\" }}' title='Validation: {{ (dataItem.PASSED_VALIDATION === \"Dirty\" ?\"" + formattedMessage + "\" : dataItem.PASSED_VALIDATION) || \"Not validated yet\" }}'></i></div></div>";
+    var tmplt = "<div class='uiControlDiv isReadOnlyCell'><div class='vert-center'><i class='valid-icon validf_{{ dataItem.PASSED_VALIDATION }} {{ (dataItem.PASSED_VALIDATION === undefined || dataItem.PASSED_VALIDATION === \"\") ? \"intelicon-protection-solid\" : (dataItem.PASSED_VALIDATION == \"Complete\") ? \"intelicon-protection-checked-verified-solid\" : \"intelicon-alert-solid\" }}' title='Validation: {{ dataItem.PASSED_VALIDATION === \"Dirty\" ?\"" + formattedMessage + "\" : dataItem.PASSED_VALIDATION || \"Not validated yet\" }}'></i></div></div>";
     return tmplt;
 }
 
