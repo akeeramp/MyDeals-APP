5c441b6771b3938970c497c510429b4a187fe360
Saurav Kundu <saurav.kundu@intel.com>
2021-03-10 14:00:46 +0530

    Revert "US931712: Vistex R2: MyDeals - Enable Rebate type "Tender" for standard Volume Tier tenders(UI)"
    This reverts commit 4263b609dc53b304cd1515260d1792ca4478f4bb.

--- Intel.MyDeals/Controllers/API/DropdownController.cs
+++ Intel.MyDeals/Controllers/API/DropdownController.cs
@@ -121,14 +121,12 @@ namespace Intel.MyDeals.Controllers.API
         public IEnumerable<BasicDropdown> GetFilteredRebateTypes(bool isTender, string dealtypeCd)
         {
             return SafeExecutor(() =>
-            {
-                var dropdowns = _dropdownLib.GetDropdowns(AttributeCodes.REBATE_TYPE, dealtypeCd);
-                return isTender
-                ? dropdowns.Where(d => d.DROP_DOWN.ToUpper() == "TENDER")
-                : (dealtypeCd == "ECAP") || (dealtypeCd == "KIT")
-                    ? dropdowns.Where(d => d.DROP_DOWN.ToUpper() != "TENDER")
-                    : dropdowns.Where(d => d.DROP_DOWN.ToUpper() != "NONE");
-            }
+                {
+                    var dropdowns = _dropdownLib.GetDropdowns(AttributeCodes.REBATE_TYPE, dealtypeCd);
+                    return isTender
+                    ? dropdowns.Where(d => d.DROP_DOWN.ToUpper() == "TENDER")
+                    : dropdowns.Where(d => d.DROP_DOWN.ToUpper() != "TENDER");
+                }
                 , $"Unable to get Dropdowns for {AttributeCodes.REBATE_TYPE} and {dealtypeCd}"
             );
         }
