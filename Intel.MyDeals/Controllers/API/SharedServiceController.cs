82aa17f462c6808f50344b5b9cfdd64b7121fc21
KrishnaX Bharath Judala <krishnax.bharath.judala@intel.com>
2021-07-29 23:24:16 +0530

    US1046675-AVM API Changes

---
+++ Intel.MyDeals/Controllers/API/SharedServiceController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Intel.Opaque;
+using System.Net.Http;
+using Intel.MyDeals.App;
+using System.Web.Http;
+using Intel.MyDeals.Entities;
+using System.Collections.ObjectModel;
+
+namespace Intel.MyDeals.Controllers.API
+{
+    [RoutePrefix("api/SharedService")]
+    public class SharedServiceController : BaseApiController
+    {
+        public OpCore op;
+        public SharedServiceController()
+        {
+            op = OpAppConfig.Init();
+        }
+
+        [Authorize]
+        [Route("getAvm")]
+        [HttpGet]
+        public avmData  getAvm()
+        {
+            avmData avmData = new avmData();
+            OpUserToken user = AppLib.InitAvm(op);
+            avmData.UserToken = user;
+            avmData.AppToken = op.AppToken.ToString();
+            avmData.AppVer = AppLib.AVM.AppVer;
+            avmData.IsDeveloper = user.IsDeveloper();
+            avmData.IsSuper = user.IsSuper();
+            avmData.IsTester = user.IsTester();
+            avmData.IsReportingUser = user.IsReportingUser();
+            avmData.IsRealSA = user.IsRealSA();
+            var idsid = user.Usr.Idsid.ToUpper();
+            avmData.UserVarticals = AppLib.UserSettings[idsid].VerticalSecurity; 
+
+            return avmData;
+        }
+
+        public class avmData
+        {
+            public OpUserToken UserToken { get; set; }
+            public string AppToken { get; set; }
+            public string AppVer { get; set; }
+            public bool IsDeveloper { get; set; }
+            public bool IsSuper { get; set; }
+            public bool IsTester { get; set; }
+            public bool IsReportingUser { get; set; }
+            public bool IsRealSA { get; set; }
+            public ObservableCollection<VerticalSecurityItem> UserVarticals { get; set; }
+
+        }
+    }
+}
