732d20f75d1a33ccedda6002cf86a9813c784c29
Ashaya Ajitha <ashaya_ta@outlook.com>
2021-11-16 15:53:11 +0530

    Messege pop-up on API trigger success

--- Intel.MyDeals/app/contract/controllers/endCustomerRetail.controller.js
+++ Intel.MyDeals/app/contract/controllers/endCustomerRetail.controller.js
@@ -6,9 +6,9 @@
 
 SetRequestVerificationToken.$inject = ['$http'];
 
-EndCustomerRetailCtrl.$inject = ['$scope', '$uibModalInstance', 'items', 'cellCurrValues', 'colName', 'country', 'dataService', 'PrimeCustomersService', '$uibModal', 'dealId', 'isAdmin'];
+EndCustomerRetailCtrl.$inject = ['$scope', '$uibModalInstance', 'items', 'cellCurrValues', 'colName', 'country', 'dataService', 'PrimeCustomersService', '$uibModal', 'dealId', 'isAdmin','logger'];
 
-function EndCustomerRetailCtrl($scope, $uibModalInstance, items, cellCurrValues, colName, country, dataService, PrimeCustomersService, $uibModal, dealId, isAdmin) {
+function EndCustomerRetailCtrl($scope, $uibModalInstance, items, cellCurrValues, colName, country, dataService, PrimeCustomersService, $uibModal, dealId, isAdmin, logger) {
     var $ctrl = this;
     $ctrl.IsError = false;
     $ctrl.ChangeErrorFlag = false;
@@ -276,24 +276,26 @@ function EndCustomerRetailCtrl($scope, $uibModalInstance, items, cellCurrValues,
             data.PRIMED_CUST_CNTRY = $ctrl.countryValues.join();
             data.END_CUSTOMER_RETAIL = $ctrl.endCustomerValues.join();
             data.END_CUST_OBJ = angular.toJson($ctrl.END_CUST_OBJ);
-            //PrimeCustomersService.UnPrimeDealsLogs(dealId, JSON.stringify(angular.toJson($ctrl.END_CUST_OBJ.filter(x => x.PRIMED_CUST_NM.toUpperCase() != "ANY"))));
             PrimeCustomersService.UnPrimeDealsLogs(dealId,JSON.stringify(angular.toJson($ctrl.END_CUST_OBJ.filter(x => x.PRIMED_CUST_NM.toUpperCase() != "ANY")))).then(
                 function (response) {
+                   
                     if (response.data == true) {
-                        logger.success("Request successfully sent to UCD.");
-                       
+                       logger.success("Request successfully sent to UCD.");
+                        
                     }
                     else {
-                        logger.error("Unable to sent UCD request.", response, response.statusText);
-                       
+                       logger.error("Unable to sent UCD request.", response, response.statusText);
+                        
                     }
-                    $uibModalInstance.close(data); 
+                    $uibModalInstance.close(data);
                 },
                 function (response) {
-                    logger.error("Unable to process UCD request.", response, response.statusText);
-                    $uibModalInstance.close(data);
+                   logger.error("Unable to process UCD request.", response, response.statusText);
+                   $uibModalInstance.close(data);
+                   
                 }
             );
+           
 
         }
     }
