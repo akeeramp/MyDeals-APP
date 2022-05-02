a43b9f8f4202a77fa02367e152b69a4891763380
akeeramp <94591893+abhilash-keerampara@users.noreply.github.com>
2022-05-02 12:17:51 +0530

    TWC-3179-709 CICD prod yml update (#774)

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -12,9 +12,9 @@ try {
        [pscustomobject]@{env='CONS2';DEPLOY_PATH='\\FM7CONMYDLW02.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM7CONMYDLW02.amr.corp.intel.com'}
        [pscustomobject]@{env='CIAR';DEPLOY_PATH='\\FM7CIAMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM7CIAMYDLW01.amr.corp.intel.com'}
        [pscustomobject]@{env='PERF';DEPLOY_PATH='\\HF2PRFMYD2W01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='HF2PRFMYD2W01.amr.corp.intel.com'}
-       [pscustomobject]@{env='DR';DEPLOY_PATH='\\HF2CINMYDLW01.amr.corp.intel.com\d$\Abhilash\DR';DEPLOY_SERVER='HF2CINMYDLW01.amr.corp.intel.com'}
-       [pscustomobject]@{env='PROD1';DEPLOY_PATH='\\HF2CINMYDLW01.amr.corp.intel.com\d$\Abhilash\PROD1';DEPLOY_SERVER='HF2CINMYDLW01.amr.corp.intel.com'}
-       [pscustomobject]@{env='PROD2';DEPLOY_PATH='\\HF2CINMYDLW01.amr.corp.intel.com\d$\Abhilash\PROD2';DEPLOY_SERVER='HF2CINMYDLW01.amr.corp.intel.com'}
+       [pscustomobject]@{env='DR';DEPLOY_PATH='\\HF2CINMYDLW01.amr.corp.intel.com\DR';DEPLOY_SERVER='HF2CINMYDLW01.amr.corp.intel.com'}
+       [pscustomobject]@{env='PROD1';DEPLOY_PATH='\\HF2CINMYDLW01.amr.corp.intel.com\PROD1';DEPLOY_SERVER='HF2CINMYDLW01.amr.corp.intel.com'}
+       [pscustomobject]@{env='PROD2';DEPLOY_PATH='\\HF2CINMYDLW01.amr.corp.intel.com\PROD2';DEPLOY_SERVER='HF2CINMYDLW01.amr.corp.intel.com'}
    )
     if ($Operation -eq 'verifyENV'){
         $result = $ENV_DATA | Where env -eq $ENV;
@@ -23,19 +23,7 @@ try {
             EXIT 1
         }
     }
-    elseif ($Operation -eq 'Deploy' ){
-     $result =  $ENV_DATA | Where env -eq $ENV;
-     if($result -eq $null){
-            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
-            EXIT 1
-        }
-        else{
-            & robocopy output/_PublishedWebsites/Intel.MyDeals $result.DEPLOY_PATH /e /MT /copyall /secfix ;
-            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
-            & robocopy output/_PublishedWebsites/Intel.MyDeals/EnvConfig/$ENV $result.DEPLOY_PATH Web.Config /MT /copyall /secfix;
-            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
-        }
-    }
+   
    elseif ($Operation -eq 'copy_latest' ){
      $result =  $ENV_DATA | Where env -eq $ENV;
      if($result -eq $null){
@@ -45,6 +33,35 @@ try {
         else{
             & robocopy output/_PublishedWebsites/Intel.MyDeals 'C:\mydeals_latest'  /e /MT /copyall /secfix ;
              if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+        }
+    }
+     elseif ($Operation -eq 'Deploy' ){
+     $result =  $ENV_DATA | Where env -eq $ENV;
+     if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else
+        {
+            & robocopy output/_PublishedWebsites/Intel.MyDeals $result.DEPLOY_PATH /e /MT /copyall /secfix ;
+            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+            & robocopy output/_PublishedWebsites/Intel.MyDeals/EnvConfig/$ENV $result.DEPLOY_PATH Web.Config /MT /copyall /secfix;
+            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+        }
+   }
+      elseif ($Operation -eq 'Deploy_prod' ){
+     $result =  $ENV_DATA | Where env -eq $ENV;
+     if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else{
+            & net use y: $result.DEPLOY_PATH /user:$USN $PWD /y;
+            & robocopy output/_PublishedWebsites/Intel.MyDeals y: /e /MT /copyall /secfix ;
+            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+            & robocopy output/_PublishedWebsites/Intel.MyDeals/EnvConfig/$ENV y: Web.Config /MT /copyall /secfix;
+            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+            & net use * /delete /y;
         }
     }
      elseif ($Operation -eq 'Deploy_latest' ){
@@ -61,6 +78,21 @@ try {
         }
    
     }
+    elseif ($Operation -eq 'Deploy_latest_prod' ){
+     $result =  $ENV_DATA | Where env -eq $ENV;
+     if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else{
+            & net use y: $result.DEPLOY_PATH /user:$USN $PWD /y;
+            & robocopy C:\mydeals_latest y: /e /MT /copyall /secfix ;
+            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+            & robocopy C:\mydeals_latest\EnvConfig\$ENV y: Web.Config /MT /copyall /secfix;
+            if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+            & net use * /delete /y;
+        }
+    }
    elseif ($Operation -eq 'IIS_Restart' ){
      $result =  $ENV_DATA | Where env -eq $ENV;
      if($result -eq $null){
@@ -72,7 +104,7 @@ try {
         $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist "$USN",$pw; 
         & Invoke-Command -computername $result.DEPLOY_SERVER -credential $cred -ScriptBlock {
             iisreset.exe
-        };
+        }
      }
     }
     else {
