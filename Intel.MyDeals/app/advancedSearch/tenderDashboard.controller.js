7d691deb9bafc3e6733cfc993087badf7dc69902
Vandana Cherukuru <100117094+cheruk1x@users.noreply.github.com>
2022-05-10 14:47:09 +0530

    TWC3167-1323: Tender Dashboard Search default Start date- reverting back to the old date (#782)

--- Intel.MyDeals/app/advancedSearch/tenderDashboard.controller.js
+++ Intel.MyDeals/app/advancedSearch/tenderDashboard.controller.js
@@ -713,12 +713,12 @@
         }
 
         $scope.$storage = $localStorage.$default({
-            //even though start date is overrided to 7 years here it is not reflecting in UI so set seperately using $localStorage.startDate
-            //startDate: moment().subtract(7, 'years').format("MM/DD/YYYY"),
+            //even though startDate changed in the below line of code it is not getting updated with the latest value so setting it using $localStorage.startDate
+            //startDate: moment().subtract(6, 'months').format("MM/DD/YYYY"),
             endDate: moment().add(6, 'months').format("MM/DD/YYYY")
         });
-        // this line is added for testing performance in DEV/ITT if start date is set 7 years back(2015) compared to the current date
-        $localStorage.startDate = moment().subtract(7, 'years').format("MM/DD/YYYY");
+
+        $localStorage.startDate = moment().subtract(6, 'months').format("MM/DD/YYYY");
         $scope.startDt = $localStorage.startDate;
         $scope.endDt = $scope.$storage.endDate;
         $scope.customers = []; //$scope.$storage.selectedCustomerIds;
