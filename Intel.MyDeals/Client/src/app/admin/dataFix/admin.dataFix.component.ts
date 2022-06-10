f6e1a0bb2f14241bae3736724f349d6153b8a9b2
Vandana Cherukuru <100117094+cheruk1x@users.noreply.github.com>
2022-06-10 15:37:30 +0530

    TWC3167-1611: Data fix Attribute builder and action builder code changes (#832)

--- Intel.MyDeals/Client/src/app/admin/dataFix/admin.dataFix.component.ts
+++ Intel.MyDeals/Client/src/app/admin/dataFix/admin.dataFix.component.ts
@@ -1,21 +1,13 @@
 ﻿import * as angular from "angular";
 import { logger } from "../../shared/logger/logger";
 import { dataFixService } from "./admin.dataFix.service";
-import { Component, ViewChild } from "@angular/core";
+import { Component } from "@angular/core";
 import { downgradeComponent } from "@angular/upgrade/static";
 import { ThemePalette } from "@angular/material/core";
-import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
-import {
-    GridDataResult,
-    DataStateChangeEvent,
-    PageSizeItem,
-} from "@progress/kendo-angular-grid";
-import {
-    process,
-    State,
-    distinct
-} from "@progress/kendo-data-query";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { GridDataResult, DataStateChangeEvent, PageSizeItem } from "@progress/kendo-angular-grid";
+import { process, State, distinct } from "@progress/kendo-data-query";
+import { forkJoin } from "rxjs";
+import { DropDownFilterSettings } from "@progress/kendo-angular-dropdowns";
 
 @Component({
     selector: "adminDataFix",
@@ -30,23 +22,24 @@ export class adminDataFixComponent {
         ).remove();
         $('link[rel=stylesheet][href="/css/kendo.intel.css"]').remove();
     }
-
     private isLoading = true;
-    private errorMsg = "";
-    private dataSource: any;
-    private gridOptions: any;
-    private allowCustom = true;
     private color: ThemePalette = "primary";
-
+    OpDataElements = [];
+    MyCustomersInfo = [];
+    AttributeSettings = [];
+    Actions = [];
     public gridResult: Array<any> = [];
-    public type = "numeric";
-    public info = true;
-    public formGroup: FormGroup;
-    public isFormChange = false;
-    private editedRowIndex: number;
-    private isDataValid: any;
-    private isNew: boolean; rowIndex: number;
-    IsEditMode = false; IsExpertMode = false;
+    IsAddMode = false; IsExpertMode = false;
+    currentDataFix = { INCDN_NBR: "", DataFixAttributes: [], DataFixActions: [], Message: "" };
+    disabled = false; JsonMessage = ""; isSuccess = false;
+    private isAtrbSelected = true; isActnSelected = true; validationError = false;
+    requiredFields = [];
+
+    MDX = [{ Text: "Modify", Value: "M" }, { Text: "Delete", Value: "D" }, { Text: "Create", Value: "X" }];
+    public filterSettings: DropDownFilterSettings = {
+        caseSensitive: false,
+        operator: "contains",
+    };
 
     public state: State = {
         skip: 0,
@@ -79,7 +72,9 @@ export class adminDataFixComponent {
 
     public gridData: GridDataResult;
 
-    distinctPrimitive(fieldName: string): any {
+    accessAllowed = true;
+
+    distinctPrimitive(fieldName: string) {
         return distinct(this.gridResult, fieldName).map(item => item[fieldName]);
     }
 
@@ -88,45 +83,133 @@ export class adminDataFixComponent {
         this.gridData = process(this.gridResult, this.state);
     }
 
+
     loadDataFix() {
         if (!((<any>window).isDeveloper)) {
             document.location.href = "/Dashboard#/portal";
         } else {
-            this.dataFixSvc.getDataFixes().subscribe(
-                (result: Array<any>) => {
+            this.dataFixSvc.getDataFixes().subscribe( (result: Array<any>) => {
                     this.gridResult = result;
                     this.gridData = process(this.gridResult, this.state);
                     this.isLoading = false;
-                },
-                function (response) {
-                    this.loggerSvc.error(
-                        "Unable to get Data Fixes.",
-                        response,
-                        response.statusText
-                    );
+            }, error=> {
+                    this.loggerSvc.error( "Unable to get Data Fixes.",error );
                 }
             );
         }
     }
 
     addNewFix() {
-        this.IsEditMode = true;
+        this.currentDataFix = {
+            INCDN_NBR: "", DataFixAttributes: [{
+                OBJ_TYPE_SID: "",
+                ATRB_SID: "",
+                ATRB_RVS_NBR: 0,
+                ATRB_MTX_SID: 0,
+                OBJ_SID: "",
+                ATRB_VAL: "",
+                ATRB_VAL_MAX: "",
+                MDX_CD: "",
+                CUST_MBR_SID: "",
+                BTCH_ID: ""
+            }], DataFixActions: [{
+                OBJ_TYPE_SID: "",
+                ACTN_NM: "",
+                ACTN_VAL_LIST: "",
+                BTCH_ID: 0
+            }], Message: ""
+        };
+        this.IsAddMode = true;
         this.IsExpertMode = false;
     }
 
     addNewFixExpert() {
-        this.IsEditMode = true;
+        this.currentDataFix = { INCDN_NBR: "", DataFixAttributes: [], DataFixActions: [], Message: "" };
+        this.IsAddMode = true;
         this.IsExpertMode = true;
     }
 
     okDataFix() {
-        this.IsEditMode = false;
+        this.isAtrbSelected = true;
+        this.isActnSelected = true;
+        this.IsAddMode = false;
         this.IsExpertMode = false;
+        this.disabled = false;
     }
 
-    okExpert() {
-        this.IsEditMode = false;
-        this.IsExpertMode = true;
+    cancelExpert() {
+        this.JsonMessage = "";
+        this.IsAddMode = false;
+        this.disabled = false;
+    }
+
+    IsValidJSONString(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+    saveNewDataFix(data, isExecute) {
+        this.dataFixSvc.updateDataFix(data, isExecute).subscribe(
+            (result) => {
+                if (result.RESULT == "1") {
+                    this.isSuccess = true;
+                    this.loggerSvc.success("Data fix has been updated successfully!");
+                    this.loadDataFix();
+                } else {
+                    this.loggerSvc.error("Unable to update data fix","");
+                }
+            }, (error) => {
+                this.loggerSvc.error("Unable to update data fix", error);
+            });
+    }
+
+    SaveFix(isExecute, isExpert) {
+        if (isExpert) {
+            this.disabled = true;
+            if (this.IsValidJSONString(this.JsonMessage)) {
+                this.saveNewDataFix(JSON.parse(this.JsonMessage), isExecute);
+            } else {
+                alert("String is not valid JSON");
+            }
+
+        } else {
+            this.currentDataFix.INCDN_NBR = this.currentDataFix.INCDN_NBR.trim();
+            this.currentDataFix.Message = this.currentDataFix.Message.trim();
+            this.currentDataFix.DataFixAttributes.map((x, index) => {
+                Object.entries(x).forEach(([key, value]) => {
+                    if (typeof value == "string") {
+                        this.currentDataFix.DataFixAttributes[index][key] = value.toString().trim();
+                    }
+                });
+            });
+            this.currentDataFix.DataFixActions.map((x, index) => {
+                Object.entries(x).forEach(([key, value]) => {
+                    if (typeof value == "string") {
+                        this.currentDataFix.DataFixActions[index][key] = value.toString().trim();
+                    }
+                });
+            });
+            if (this.currentDataFix.INCDN_NBR === "")
+                this.requiredFields.push("Incident Number");
+            if (this.isAtrbSelected && isExecute && this.currentDataFix.DataFixAttributes.filter(x => x.OBJ_TYPE_SID === "" || x.OBJ_SID === "" || x.OBJ_SID === "0" || x.MDX_CD === "" || x.CUST_MBR_SID === "" || x.ATRB_RVS_NBR == null).length > 0)
+                this.requiredFields.push("Mandatory data in attributes section cannot be empty");
+            if (this.isActnSelected && isExecute && this.currentDataFix.DataFixActions.filter(x => x.OBJ_TYPE_SID === "" || x.ACTN_NM === "").length > 0)
+                this.requiredFields.push("Mandatory data in actions section cannot be empty");
+            const regExpForObjectIds = /[0-9,]+$/;
+            if (this.currentDataFix.DataFixActions.filter(x => x.ACTN_VAL_LIST !== "" && !regExpForObjectIds.exec(x.ACTN_VAL_LIST)).length > 0)
+                this.requiredFields.push("Target object IDs in actions has illegal characters!");
+            if (this.requiredFields.length > 0) {
+                this.validationError = true;
+            } else {
+                this.disabled = true;
+                this.validationError = false;
+                this.saveNewDataFix(this.currentDataFix, isExecute);
+            }
+        }
     }
 
     refreshGrid() {
@@ -138,10 +221,71 @@ export class adminDataFixComponent {
         this.loadDataFix();
     }
 
+    close() {
+        this.validationError = false;
+        this.requiredFields = [];
+    }
 
     ngOnInit() {
-        this.loadDataFix();
+        if (!(<any>window).isDeveloper) {
+            // Kick not valid users out of the page 
+            this.accessAllowed = false;
+            document.location.href = "/Dashboard#/portal";
+        } else {
+            this.loadDataFix();
+            forkJoin({
+                MyCustomersInfo: this.dataFixSvc.getMyCustomersNameInfo(),
+                OpDataElements: this.dataFixSvc.getOpDataElements(),
+                AttributeSettings: this.dataFixSvc.getDataFixActions(),
+                Actions: this.dataFixSvc.getDataFixActions()
+            }).subscribe(({ MyCustomersInfo, OpDataElements, AttributeSettings, Actions }) => {
+                this.MyCustomersInfo = MyCustomersInfo;
+                this.OpDataElements = OpDataElements;
+                this.AttributeSettings = AttributeSettings.filter((x) => x.DdlType === 'ATRB_LIST');
+                this.Actions = Actions.filter(x => x.DdlType === "ACTN_LIST");
+
+            }
+                , error => {
+                    this.loggerSvc.error("Unable to getVendorDropDown.", error);
+                });
+        }
     }
+
+    addRow(dataFixBuilder, index) {
+        if (index > -1) {
+            if (dataFixBuilder == "ActnBuilder") {
+                this.currentDataFix.DataFixActions.splice(index + 1, 0, {
+                    OBJ_TYPE_SID: "",
+                    ACTN_NM: "",
+                    ACTN_VAL_LIST: "",
+                    BTCH_ID: 0
+                });
+            }
+            else {
+                this.currentDataFix.DataFixAttributes.splice(index + 1, 0, {
+                    OBJ_TYPE_SID: "",
+                    ATRB_SID: 0,
+                    ATRB_RVS_NBR: 0,
+                    ATRB_MTX_SID: 0,
+                    OBJ_SID: "",
+                    ATRB_VAL: "",
+                    ATRB_VAL_MAX: "",
+                    MDX_CD: "",
+                    CUST_MBR_SID: "",
+                    BTCH_ID: ""
+                });
+            }
+        }
+    }
+
+    removeRow(dataFixBuilder, index) {
+        if (index > -1) {
+            if (dataFixBuilder == "ActnBuilder") this.currentDataFix.DataFixActions.splice(index, 1);
+            else this.currentDataFix.DataFixAttributes.splice(index, 1);
+
+        }
+    }
+
     ngOnDestroy() {
         //The style removed are adding back
         $('head').append('<link rel="stylesheet" type="text/css" href="/Content/kendo/2017.R1/kendo.common-material.min.css">');
@@ -155,3 +299,4 @@ angular
         "adminDataFix",
         downgradeComponent({ component: adminDataFixComponent })
     );
+
