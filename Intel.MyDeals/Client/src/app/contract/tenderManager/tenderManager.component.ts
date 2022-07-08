aa8e2d7c4b770864f69c04685f82d41fe21daa55
anushadx <100208086+anushadx@users.noreply.github.com>
2022-07-08 14:57:23 +0530

    TWC3167-1750-Tender manager landing page (#874)

--- Intel.MyDeals/Client/src/app/contract/tenderManager/tenderManager.component.ts
+++ Intel.MyDeals/Client/src/app/contract/tenderManager/tenderManager.component.ts
@@ -1,18 +1,62 @@
 ﻿import * as angular from "angular";
 import { Component } from "@angular/core";
 import { downgradeComponent } from "@angular/upgrade/static";
+import { logger } from "../../shared/logger/logger";
+import { pricingTableservice } from "../pricingTable/pricingTable.service";
+import { templatesService } from "../../shared/services/templates.service";
 
 @Component({
     selector: "tenderManager",
     templateUrl: "Client/src/app/contract/tenderManager/tenderManager.component.html",
+    styleUrls: ["Client/src/app/contract/tenderManager/tenderManager.component.css"]
 })
 
 export class tenderManagerComponent {
-    public c_Id: number;
+    constructor(private loggerSvc: logger, private pricingTableSvc: pricingTableservice, private templatesSvc: templatesService) { }
+    public c_Id: any = '';
+    public ps_Id: any = '';
+    public pt_Id: any = '';
+    public contractData = null;
+    public selectedTab = "PTR";
+    public UItemplate = null;
+    public isLoading = true;
+
+    loadAllContractDetails() {
+        this.pricingTableSvc.readContract(this.c_Id).subscribe((response: Array<any>) => {
+            this.contractData = response[0];
+            this.ps_Id = this.contractData.PRC_ST[0].DC_ID;
+            this.pt_Id = this.contractData.PRC_ST[0].PRC_TBL[0].DC_ID;
+            this.templatesSvc.readTemplates().subscribe((response: Array<any>) => {
+                this.UItemplate = response;
+                this.isLoading = false;
+            }, (error) => {
+                this.loggerSvc.error('loadAllContractDetails::readTemplates:: service', error);
+                this.isLoading = false;
+            })
+        }, (error) => {
+            this.loggerSvc.error('loadAllContractDetails::readContract:: service', error);
+            this.isLoading = false;
+        })
+
+    }
+
+    tenderWidgetPathManager(data, selectedTab) {
+        if (selectedTab == 'PD') {
+            window.location.href = "/advancedSearch#/tenderDashboard";
+        }
+        this.selectedTab = selectedTab;
+        this.loadAllContractDetails();
+
+    }
+
+    isPTRPartiallyComplete(selectedTab) {
+        //need to implement validation logic here for progress bar status
+    }
 
     ngOnInit() {
         const url = window.location.href.split('/');
         this.c_Id = Number(url[url.length - 1]);
+        this.loadAllContractDetails();
     }
 }
 
@@ -22,4 +66,3 @@ angular.module("app").directive(
         component: tenderManagerComponent,
     })
 );
-
