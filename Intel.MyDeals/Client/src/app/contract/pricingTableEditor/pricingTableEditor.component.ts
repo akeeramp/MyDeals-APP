2463527cde6e76dc203a8ba039fd9f94e083168d
prave16x <112366955+prave16x@users.noreply.github.com>
2022-12-08 23:21:24 +0530

    TWC3167-3249, TWC3167-3147, TWC3167-3199 : UI Bug Fixes (#1454)

--- Intel.MyDeals/Client/src/app/contract/pricingTableEditor/pricingTableEditor.component.ts
+++ Intel.MyDeals/Client/src/app/contract/pricingTableEditor/pricingTableEditor.component.ts
@@ -520,7 +520,7 @@ export class pricingTableEditorComponent {
             }
             this.columns.push(currentColumnConfig);
             nestedHeaders[0].push(sheetObj[index]);
-            nestedHeaders[1].push(`<span style="color: rgb(0, 60, 113);font-family:'Intel Clear';font-weight: bold;font-size: 13px;">${item.title}</span>`);
+            nestedHeaders[1].push(`<span style="color: rgb(0, 60, 113);font-family:'Intel Clear';font-weight: bold;font-size: 13px;opacity: .7;">${item.title}</span>`);
         });
         /* Hidden Columns */
         hiddenColumns = PTE_Load_Util.getHiddenColumns(columnTemplates, this.contractData.CustomerDivisions);
