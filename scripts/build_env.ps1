9504486563a92ab6750b1e73b1d043313d4b8459
Sudeepx <100108869+Sudeepx@users.noreply.github.com>
2023-01-11 11:42:42 +0530

    TWC3167-4198 : Pipeline Readiness - Pushing changes to RLS branch (#1694)

---
+++ scripts/build_env.ps1
@@ -0,0 +1,48 @@
+param([string] $Operation,$PATH,$DIR)
+
+try {
+    if ($Operation -eq 'nuget'){
+      & $PATH restore;
+    }
+    elseif ($Operation -eq 'NPMInstall' ){
+        Set-Location -Path Intel.MyDeals/Client/ -PassThru;
+        npm install;
+        Set-Location -Path .. -PassThru ; 
+        Set-Location -Path ..  -PassThru;
+    }
+      elseif ($Operation -eq 'NPMBuild' ){
+        Set-Location -Path Intel.MyDeals/Client/ -PassThru;
+        npm run build_env;
+        Set-Location -Path .. -PassThru ; 
+        Set-Location -Path ..  -PassThru;
+    }
+    elseif ($Operation -eq 'MSBuild' ){
+     & $PATH /p:OutDir="$DIR/output" /p:TransformConfigFiles=true /p:Configuration=Release /p:Platform="Any CPU" /p:GenerateSerializationAssemblies=Off;
+    }
+
+    elseif ($Operation -eq 'MovePublish' ){
+     & robocopy output/_PublishedWebsites/Intel.MyDeals $PATH /e /MT /copyall /secfix ;
+     if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+     & robocopy output/_PublishedWebsites/Intel.MyDeals/EnvConfig/DEV $PATH Web.Config /MT /copyall /secfix;
+     if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+    }
+    elseif ($Operation -eq 'ClientZip' ){
+     if (Test-Path "C:\ClientZip\Client.zip") {Remove-Item -Path "C:\ClientZip\Client.zip"};
+     Add-Type -assembly "system.io.compression.filesystem";[io.compression.zipfile]::CreateFromDirectory((Get-Location).path+"\Intel.MyDeals\Client\", "C:\ClientZip\Client.zip");
+    }
+    elseif ($Operation -eq 'ClientZip_latest' ){
+     if (Test-Path "C:\ClientZip\Client.zip") {Remove-Item -Path "C:\ClientZip\Client.zip"};
+     Add-Type -assembly "system.io.compression.filesystem";[io.compression.zipfile]::CreateFromDirectory("C:\mydeals_latest_angular\Client", "C:\ClientZip\Client.zip");
+    }
+    else {
+        Write-Host "Please provide correct values" -BackgroundColor DarkRed
+        EXIT 1
+    }
+
+}
+catch {
+    # Get the current error
+    Write-Host $_.Exception.Message -BackgroundColor DarkRed
+    EXIT 1
+}
+
---
+++ scripts/deploy_env.ps1
@@ -0,0 +1,165 @@
+param([string] $Operation,$USN,$PWD,$SERVER)
+
+try {
+
+ $ENV_DATA = @(
+        [pscustomobject]@{env='CIN';DEPLOY_PATH='\\HF2CINMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='HF2CINMYDLW01.amr.corp.intel.com';pool="cinmydeals.inte.com";config="DEV"}
+        [pscustomobject]@{env='DEV';DEPLOY_PATH='\\HF2DEVMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='HF2DEVMYDLW01.amr.corp.intel.com';pool="devmydeals.inte.com";config="DEV"}
+        [pscustomobject]@{env='ITT';DEPLOY_PATH='\\HF2ITTMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='HF2ITTMYDLW01.amr.corp.intel.com';pool="ittmydeals.intel.com";config="ITT"}
+        [pscustomobject]@{env='EUT';DEPLOY_PATH='\\hf2cinmydlw01.amr.corp.intel.com\MyDealsEUT';DEPLOY_SERVER='hf2cinmydlw01.amr.corp.intel.com';pool="eutmydeals.intel.com";config="EUT"}
+        [pscustomobject]@{env='DAY1';DEPLOY_PATH='\\hf2cinmydlw01.amr.corp.intel.com\MyDealsDay1';DEPLOY_SERVER='hf2cinmydlw01.amr.corp.intel.com';pool="mydeals-day1.intel.com";config="DAY1"}
+        [pscustomobject]@{env='UTT1';DEPLOY_PATH='\\HF2UTTMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='HF2UTTMYDLW01.amr.corp.intel.com';pool="uttmydeals.intel.com";config="UTT"}
+        [pscustomobject]@{env='UTT2';DEPLOY_PATH='\\HF2UTTMYDLW02.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='HF2UTTMYDLW02.amr.corp.intel.com';pool="uttmydeals.intel.com";config="UTT"}
+        [pscustomobject]@{env='CONS1';DEPLOY_PATH='\\FM7CONMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM7CONMYDLW01.amr.corp.intel.com';pool="conmydeals.intel.com";config="CONS"}
+        [pscustomobject]@{env='CONS2';DEPLOY_PATH='\\FM7CONMYDLW02.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM7CONMYDLW02.amr.corp.intel.com';pool="conmydeals.intel.com";config="CONS"}
+        [pscustomobject]@{env='CIAR';DEPLOY_PATH='\\FM7CIAMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM7CIAMYDLW01.amr.corp.intel.com';pool="ciamydeals.intel.com";config="CIAR"}
+        [pscustomobject]@{env='PERF';DEPLOY_PATH='\\HF2PRFMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='HF2PRFMYDLW01.amr.corp.intel.com';pool="perfmydeals.intel.com";config="PERF"}
+        [pscustomobject]@{env='DR';DEPLOY_PATH='\\CH2DRMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='CH2DRMYDLW01.amr.corp.intel.com';pool="drmydeals.intel.com";config="PROD"}
+        [pscustomobject]@{env='PROD1';DEPLOY_PATH='\\FM1PRDMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM1PRDMYDLW01.amr.corp.intel.com';pool="prdmydeals.intel.com";config="PROD"}
+        [pscustomobject]@{env='PROD2';DEPLOY_PATH='\\FM1PRDMYDLW02.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM1PRDMYDLW02.amr.corp.intel.com';pool="prdmydeals.intel.com";config="PROD"}
+        [pscustomobject]@{env='PRODA1';DEPLOY_PATH='\\FM1PRODMYDLW01.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM1PRODMYDLW01.amr.corp.intel.com';pool="prdmydeals.intel.com";config="CONS"}
+        [pscustomobject]@{env='PRODA2';DEPLOY_PATH='\\FM1PRODMYDLW02.amr.corp.intel.com\MyDeals';DEPLOY_SERVER='FM1PRODMYDLW02.amr.corp.intel.com';pool="prdmydeals.intel.com";config="CONS"}
+   )
+    if ($Operation -eq 'verifyENV'){
+        $result = $ENV_DATA | Where env -eq $SERVER;
+        if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+    }
+     elseif ($Operation -eq 'MovePublishENV' ){
+     $result = $ENV_DATA | Where env -eq $SERVER;
+      if($result -eq $null){
+              Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+              EXIT 1
+          }
+      else {
+      $config=$result.config
+      & robocopy output/_PublishedWebsites/Intel.MyDeals $result.DEPLOY_PATH /e /MT /copyall /secfix ;
+      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+      & robocopy output/_PublishedWebsites/Intel.MyDeals/EnvConfig/$config $result.DEPLOY_PATH Web.Config /MT /copyall /secfix;
+      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+      }
+    }
+    elseif ($Operation -eq 'MovePublish_latest' ){
+     $result = $ENV_DATA | Where env -eq $SERVER;
+      if($result -eq $null){
+              Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+              EXIT 1
+          }
+      else {
+        $config=$result.config
+      & robocopy C:\mydeals_latest_angular  $result.DEPLOY_PATH /e /MT /copyall /secfix ;
+      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+      & robocopy C:\mydeals_latest_angular\EnvConfig\$config $result.DEPLOY_PATH Web.Config /MT /copyall /secfix;
+      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+      }
+    }
+       elseif ($Operation -eq 'MovePublish_latest_prod' ){
+     $result = $ENV_DATA | Where env -eq $SERVER;
+      if($result -eq $null){
+              Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+              EXIT 1
+          }
+      else {
+        $config=$result.config
+      & robocopy C:\mydeals_ciar_latest_angular  $result.DEPLOY_PATH /e /MT /copyall /secfix ;
+      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+      & robocopy C:\mydeals_ciar_latest_angular\EnvConfig\$config $result.DEPLOY_PATH Web.Config /MT /copyall /secfix;
+      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+      }
+    }
+
+    elseif ($Operation -eq 'RemoveZipENV' ){
+        $result = $ENV_DATA | Where env -eq $SERVER;
+        if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else{
+           $pw = convertto-securestring -AsPlainText -Force -String "$PWD";
+            $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist "$USN",$pw; 
+            & Invoke-Command -computername  $result.DEPLOY_SERVER -credential $cred -ScriptBlock {if (Test-Path "D:\WebSites\MyDeals\Client.zip") {Remove-Item -Path "D:\WebSites\MyDeals\Client.zip"}};
+        }
+   }
+   elseif ($Operation -eq 'CopyZipENV' ){
+       $result = $ENV_DATA | Where env -eq $SERVER;
+        if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else{
+        & Copy-Item "C:\ClientZip\Client.zip" $result.DEPLOY_PATH -Force;
+        }
+    }
+    elseif ($Operation -eq 'DeployENV' ){
+          $result = $ENV_DATA | Where env -eq $SERVER;
+          $pool = $result.pool;
+          if($result -eq $null){
+              Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+              EXIT 1
+          }
+          else{
+            $pw = convertto-securestring -AsPlainText -Force -String "$PWD";
+            $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist "$USN",$pw; 
+            & Invoke-Command -computername $result.DEPLOY_SERVER -credential $cred -ScriptBlock { param($pool)
+              Remove-Item "D:\WebSites\MyDeals\Client\*" -Force -Recurse;
+              Add-Type -assembly "system.io.compression.filesystem";[io.compression.zipfile]::ExtractToDirectory("D:\WebSites\MyDeals\Client.zip", "D:\WebSites\MyDeals\Client\");
+              Restart-WebAppPool -Name  $pool
+          } -Argumentlist $pool
+     }
+    }
+    elseif ($Operation -eq 'copy_latestENV' ){
+     $result =  $ENV_DATA | Where env -eq $SERVER;
+     if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else{
+            & robocopy output/_PublishedWebsites/Intel.MyDeals 'C:\mydeals_latest_angular'  /e /MT /copyall /secfix ;
+             if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+            & Copy-Item "C:\ClientZip\Client.zip" 'C:\mydeals_latest_angular' -Force;
+        }
+    }
+     elseif ($Operation -eq 'copy_latest_prodENV' ){
+     $result =  $ENV_DATA | Where env -eq $SERVER;
+     if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else{
+            if($SERVER -eq 'CIAR') {
+             & robocopy output/_PublishedWebsites/Intel.MyDeals 'C:\mydeals_ciar_latest_angular'  /e /MT /copyall /secfix ;
+             if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
+            & Copy-Item "C:\ClientZip\Client.zip" 'C:\mydeals_ciar_latest_angular' -Force;
+            }
+        }
+    }
+    elseif ($Operation -eq 'Deploy_latest' ){
+     $result =  $ENV_DATA | Where env -eq $SERVER;
+     $pool = $result.pool;
+     if($result -eq $null){
+            Write-Host "Please provide correct values for environment" -BackgroundColor DarkRed
+            EXIT 1
+        }
+        else{
+            $pw = convertto-securestring -AsPlainText -Force -String "$PWD";
+            $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist "$USN",$pw; 
+            & Invoke-Command -computername $result.DEPLOY_SERVER -credential $cred -ScriptBlock {param($pool)
+              Remove-Item "D:\WebSites\MyDeals\Client\*" -Force -Recurse;
+              Add-Type -assembly "system.io.compression.filesystem";[io.compression.zipfile]::ExtractToDirectory("D:\WebSites\MyDeals\Client.zip", "D:\WebSites\MyDeals\Client\");
+              Restart-WebAppPool -Name $pool
+        } -Argumentlist $pool 
+    }
+    else {
+        Write-Host "Please provide correct values" -BackgroundColor DarkRed
+        EXIT 1
+    }
+ }
+}
+catch {
+    # Get the current error
+    Write-Host $_.Exception.Message -BackgroundColor DarkRed
+    EXIT 1
+}
+
