a09f7780ea19f13dd5297d34280c459918c9d009
abhilash-keerampara <abhilash.keerampara@intel.com>
2023-01-13 11:04:15 +0530

    TWC3179-XXX: build.ps1 alignment change

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -10,13 +10,13 @@ try {
         Set-Location -Path .. -PassThru ; 
         Set-Location -Path ..  -PassThru;
     }
-      elseif ($Operation -eq 'NPMBuild' ){
+    elseif ($Operation -eq 'NPMBuild' ){
         Set-Location -Path Intel.MyDeals/Client/ -PassThru;
         npm run build_env;
         Set-Location -Path .. -PassThru ; 
         Set-Location -Path ..  -PassThru;
     }
-         elseif ($Operation -eq 'LICENSE_Activate' ){
+    elseif ($Operation -eq 'LICENSE_Activate' ){
         Set-Location -Path Intel.MyDeals/Client/ -PassThru;
         npx kendo-ui-license activate;
         Set-Location -Path .. -PassThru ; 
@@ -25,14 +25,13 @@ try {
     elseif ($Operation -eq 'MSBuild' ){
      & $PATH /p:OutDir="$DIR/output" /p:TransformConfigFiles=true /p:Configuration=Release /p:Platform="Any CPU" /p:GenerateSerializationAssemblies=Off;
     }
-
     elseif ($Operation -eq 'MovePublish' ){
      & robocopy output/_PublishedWebsites/Intel.MyDeals $PATH /e /MT /copyall /secfix ;
      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
      & robocopy output/_PublishedWebsites/Intel.MyDeals/EnvConfig/DEV $PATH Web.Config /MT /copyall /secfix;
      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
     }
-     elseif ($Operation -eq 'MovePublishUTT' ){
+    elseif ($Operation -eq 'MovePublishUTT' ){
      & robocopy output/_PublishedWebsites/Intel.MyDeals $PATH /e /MT /copyall /secfix ;
      if ($lastexitcode -lt 8) { $global:LASTEXITCODE = $null };
      & robocopy output/_PublishedWebsites/Intel.MyDeals/EnvConfig/UTT $PATH Web.Config /MT /copyall /secfix;
@@ -51,14 +50,13 @@ try {
     elseif($Operation -eq 'SendStartNotification'){
          node C:\automation_newman\buildmailer.js Start $PATH
     }
-        elseif($Operation -eq 'SendEndNotification'){
+    elseif($Operation -eq 'SendEndNotification'){
          node C:\automation_newman\buildmailer.js Stop $PATH
     }
     else {
         Write-Host "Please provide correct values" -BackgroundColor DarkRed
         EXIT 1
     }
-
 }
 catch {
     # Get the current error
