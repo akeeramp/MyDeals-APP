adf69e69bfe55eed92271ec1c16887fa5897c6b3
gsagarix <107667415+gsagarix@users.noreply.github.com>
2023-05-11 14:17:26 +0530

    TWC3167-5732 : Under manage deals tab- start date i icon css fix. (#2119)

--- Intel.MyDeals/Client/src/app/contract/grid.util.ts
+++ Intel.MyDeals/Client/src/app/contract/grid.util.ts
@@ -240,8 +240,8 @@ export class GridUtil {
         var tmplt = '';
         if (passedData._behaviors != undefined && passedData._behaviors.isError != undefined && passedData._behaviors.isError[field] != undefined)
             tmplt += '<div class="err-bit" kendoTooltip title="' + passedData._behaviors.validMsg[field] + '"></div>';
-        tmplt += '<div class="uiControlDiv dealCell ' + this.getClassNm(passedData, field) + ' ' + msgClass + '" ' + finalMsg + '>';
-        tmplt += '    <div class="ng-binding vert-center" style="padding-left: 10px !important;">';
+        tmplt += '<div class="uiControlDiv dealCell class_date ' + this.getClassNm(passedData, field) + ' ' + msgClass + '" ' + finalMsg + '>';
+        tmplt += '    <div class="ng-binding vert-center" style="padding-left:0px !important;white-space:nowrap">';
         if (this.displayFrontEndDateMessage(passedData))
             tmplt += '<span class="vert-center" style="top: 20% !important"> <i class="intelicon-information dateWrapper" title="If the deal start date is in the past, the deal start date will change to the date when the deal becomes active."></i> </span>'
         if (passedData[field] != undefined && passedData[field] != null)
