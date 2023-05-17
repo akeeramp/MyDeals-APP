9f1c289519dd7f59cfe0377de73a3e2405ea6df5
mittal-monisha <99460317+mittal-monisha@users.noreply.github.com>
2023-05-17 15:44:15 +0530

    TWC3179-2275: Health check API for New Relic alerts implementation (#2144)

---
+++ Intel.MyDeals/Controllers/API/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Intel.MyDeals.Controllers.API
+{
+    [RoutePrefix("api/HealthCheck")]
+    public class HealthCheckController : ApiController
+    {
+        [Route("getstatus")]
+        [HttpGet]
+        public string GetHealthCheckStatus()
+        {
+            return "API Response Status Code: 200 OK";
+        }
+    }
+}
