78babe298517fb00a8e1ea6f255459592f93bf0e
gsagarix <107667415+gsagarix@users.noreply.github.com>
2023-05-25 15:00:32 +0530

    TWC3167-6107,TWC3167-6093 : Word wrap fix for manage - deals tab & contract backdate reason popup fix (#2171)

--- Intel.MyDeals/Client/src/app/contract/grid.util.ts
+++ Intel.MyDeals/Client/src/app/contract/grid.util.ts
@@ -43,7 +43,7 @@ export class GridUtil {
             if (passedData._behaviors != undefined && passedData._behaviors.isError != undefined && passedData._behaviors.isError[field])
                 tmplt = '<div class="err-bit" kendoTooltip title="' + passedData._behaviors.validMsg[field] + '"></div>';
             if (passedData[field] != undefined && passedData[field] != null)
-                tmplt += '    <div class="ng-binding vert-center ' + msgClass + '" ' + finalMsg + '>' + passedData[field] + '</div>';
+                tmplt += '    <div class="ng-binding vert-center wordWrapTransform' + msgClass + '" ' + finalMsg + '>' + passedData[field] + '</div>';
             tmplt += '</div>';
             return tmplt;
         }
