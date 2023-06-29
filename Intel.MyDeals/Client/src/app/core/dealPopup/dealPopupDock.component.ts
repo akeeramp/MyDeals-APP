36e830ef60af49dc8b643af9258cd203dbdb885b
Jesus Lopez Reynosa <jesus.lopez.reynosa@intel.com>
2023-06-29 15:45:13 -0700

    TWC3119-667: Fixed horizontal alignment of Quick Deal Popup (#2252)

--- Intel.MyDeals/Client/src/app/core/dealPopup/dealPopupDock.component.ts
+++ Intel.MyDeals/Client/src/app/core/dealPopup/dealPopupDock.component.ts
@@ -1,15 +1,17 @@
 ﻿import { Component, HostListener } from "@angular/core"; 
+import { isNull, isUndefined } from "underscore";
+
 import { AppEvent, broadCastService } from "./broadcast.service";
 import { quickDealConstants } from "../angular.constants";
 
 @Component({
-    selector: "deal-popup-dock-angular",
-    templateUrl: "Client/src/app/core/dealPopup/dealPopupDock.component.html", 
+    selector: 'deal-popup-dock-angular',
+    templateUrl: 'Client/src/app/core/dealPopup/dealPopupDock.component.html', 
 })
 export class dealPopupDockComponent  {
     ids: any= [];
     recent: any = []; 
-    menuOrientation = "horizontal"; 
+    menuOrientation = 'horizontal'; 
     storage = localStorage;
     maxItems: any;
     maxRecent: any;
@@ -18,7 +20,8 @@ export class dealPopupDockComponent  {
     screenWidth: number;
     errormsg = "";
     ismaxDealsOpened= false;
-    constructor(private brdcstservice: broadCastService) {
+
+    constructor(private broadcastService: broadCastService) {
         this.getScreenSize();
     }
 
@@ -27,11 +30,12 @@ export class dealPopupDockComponent  {
         this.screenHeight = window.innerHeight;
         this.screenWidth = window.innerWidth; 
     }
+
     intializedealpopupDock() {
-        this.brdcstservice.on("QuickDealToggleDeal").subscribe(event => {
+        this.broadcastService.on("QuickDealToggleDeal").subscribe(event => {
             this.subscribeEvents(event.payload);
         });
-        this.brdcstservice.on("QuickDealWidgetClosed").subscribe(event => {
+        this.broadcastService.on("QuickDealWidgetClosed").subscribe(event => {
             this.subscribeEvents(event.payload);
         });
 
@@ -42,71 +46,70 @@ export class dealPopupDockComponent  {
         if (window.navigator.userAgent.indexOf("Chrome") < 0) this.isEnabled = false;
         this.ids = JSON.parse(this.storage.getItem('dealPopupDockData'));
 
-        if (this.ids === undefined || this.ids === null) {
+        if (isUndefined(this.ids) || isNull(this.ids)) {
             this.ids = [];
         }
         this.recent = JSON.parse(this.storage.getItem('dealPopupDockRecentData'));
-        if (this.recent === undefined || this.recent === null) this.recent = [];
+        if (isUndefined(this.recent) || isNull(this.recent)) this.recent = [];
     }
      
-    //save
     save() {
         this.storage.setItem('dealPopupDockData', JSON.stringify(this.ids));
         this.storage.setItem('dealPopupDockRecentData', JSON.stringify(this.recent));
-        const recentdeals = JSON.parse(this.storage.getItem('dealPopupDockRecentData'));
-        if (recentdeals.length > this.maxRecent) {
-            recentdeals.pop();
-            this.storage.setItem('dealPopupDockRecentData', JSON.stringify(recentdeals));
+        const RECENT_DEALS = JSON.parse(this.storage.getItem('dealPopupDockRecentData'));
+        if (RECENT_DEALS.length > this.maxRecent) {
+            RECENT_DEALS.pop();
+            this.storage.setItem('dealPopupDockRecentData', JSON.stringify(RECENT_DEALS));
         }
     } 
 
     addIdBase(id, top, left, isOpen) {
-         if (isOpen === undefined) isOpen = false;
+        if (isUndefined(isOpen)) isOpen = false;
 
-         let found = false;
-         for (let i = 0; i < this.ids.length; i++) {
-             if (this.ids[i].id === id) {
-                 found = true;
-                 this.ids[i].top = top;
-                 this.ids[i].left = left;
-             }
-         }
-         if (!found) {
-             this.ids.push({
-                 id: id,
-                 top: top,
-                 left: left,
-                 isOpen: isOpen,
-                 position: {
-                     x: 0,
-                     y: 0
-                 }
-             });
-         }
+        let found = false;
+        for (let i = 0; i < this.ids.length; i++) {
+            if (this.ids[i].id === id) {
+                found = true;
+                this.ids[i].top = top;
+                this.ids[i].left = left;
+            }
+        }
+        if (!found) {
+            this.ids.push({
+                id: id,
+                top: top,
+                left: left,
+                isOpen: isOpen,
+                position: {
+                    x: 0,
+                    y: 0
+                }
+            });
+        }
 
-         let foundRecent = false;
-         for (let i = 0; i < this.recent.length; i++) {
-             if (this.recent[i].id === id) {
-                 foundRecent = true;
-                 this.recent[i].top = top;
-                 this.recent[i].left = left;
-             }
-         }
+        let foundRecent = false;
+        for (let i = 0; i < this.recent.length; i++) {
+            if (this.recent[i].id === id) {
+                foundRecent = true;
+                this.recent[i].top = top;
+                this.recent[i].left = left;
+            }
+        }
 
-         if (!foundRecent) {
-             this.recent.push({
-                 id: id,
-                 top: top,
-                 left: left,
-                 isOpen: false,
-                 position: {
-                     x: 0,
-                     y: 0
-                 }
-             });
-         }
-         this.save();
-     }
+        if (!foundRecent) {
+            this.recent.push({
+                id: id,
+                top: top,
+                left: left,
+                isOpen: false,
+                position: {
+                    x: 0,
+                    y: 0
+                }
+            });
+        }
+        this.save();
+    }
 
     addId(id, top, left) {
         this.addIdBase(id, top, left, undefined);
@@ -122,16 +125,17 @@ export class dealPopupDockComponent  {
                 this.ids.splice(i, 1);
             }
         }
-        this.storage.setItem('dealPopupDockData', JSON.stringify(this.ids));
-     }
 
-    recentClicked(event, id) {
-        const x = event.clientX + 90;
-        const y = event.clientY + 2; 
-        this.QuickDealToggle("QuickDealToggleDeal",id,y,x);
+        this.storage.setItem('dealPopupDockData', JSON.stringify(this.ids));
     }
 
-    QuickDealToggle(Event, id, y, x) {
+    recentClicked(event, id) {
+        const X = event.clientX + 90;
+        const Y = event.clientY + 2; 
+        this.quickDealToggle("QuickDealToggleDeal",id,Y,X);
+    }
+
+    quickDealToggle(event, id, y, x) {
         if (this.ids.length >= this.maxItems) {
             this.ismaxDealsOpened = true;
             this.errormsg = "Only " + this.maxItems + " Quick Deals can be opened at one time. Please close one before trying to open this deal.";
@@ -154,7 +158,7 @@ export class dealPopupDockComponent  {
 
     subscribeEvents(result) {
         if (result.key == "QuickDealToggleDeal") {
-            this.QuickDealToggle(result.key, result.id, result.y, result.x);
+            this.quickDealToggle(result.key, result.id, result.y, result.x);
         } else if (result.key == "QuickDealWidgetOpened") {
             this.addId(result.id, result.y, result.x);
         } else if (result.key == "QuickDealWidgetClosed") {
@@ -168,71 +172,72 @@ export class dealPopupDockComponent  {
         this.ids = [];
         this.storage.setItem('dealPopupDockData', JSON.stringify(this.ids));
     }
+
     closeAction() {
         this.ismaxDealsOpened = false;
     }
 
     cascadeAll() {
-        const offset = 25;
-        const initX =5;
-        const initY = 20;
+        const OFFSET = 25;
+        const INIT_X = 75;
+        const INIT_Y = 30;
         for (let i = 0; i < this.ids.length; i++) {
           const item = this.ids[i]; 
-            const l = initX + (i * offset);
-            const t = initY + (i * offset);
-            item.left = l;
-            item.top = t;
-            item.position = { x: l, y: t }; 
+            const LEFT = INIT_X + (i * OFFSET);
+            const TOP = INIT_Y + (i * OFFSET);
+            item.left = LEFT;
+            item.top = TOP;
+            item.position = { x: LEFT, y: TOP }; 
         }      
         this.save();
-        this.brdcstservice.dispatch(new AppEvent("QuickDealShowPanel", "showpanel"));
+        this.broadcastService.dispatch(new AppEvent("QuickDealShowPanel", "showpanel"));
     }
 
     lowerAll() {
-       
-        const initX = 5;
+        const INIT_X = 75;
         let initY = 0;
         if (this.screenHeight > 890) {
-            initY = this.screenHeight - 600;
+            initY = this.screenHeight - 800;
         } else {
-            initY = this.screenHeight - 480;
-
+            initY = this.screenHeight - 520;
         }
-        const offsetX = 100;
+
+        const OFFSET_X = 100;
         for (let i = 0; i < this.ids.length; i++) {
-            const item = this.ids[i];
-             const l = initX + (i * offsetX);
-            item.left = l;
-            item.top = initY;
-            item.position = { x: l, y: initY }; 
+            const ITEM = this.ids[i];
+            const LEFT = INIT_X + (i * OFFSET_X);
+            ITEM.left = LEFT;
+            ITEM.top = initY;
+            ITEM.position = { x: LEFT, y: initY }; 
         }
 
-        this.brdcstservice.dispatch(new AppEvent("QuickDealClosePanel", "hidepanel"));
+        this.broadcastService.dispatch(new AppEvent("QuickDealClosePanel", "hidepanel"));
     }
-    tileAll() { 
-        const offsetX = 300;
-        const offsetY = 120;
-        const initX = 5;
-        const initY = 5;
-        const docWidth = this.screenWidth-800;
+
+    tileAll() {
+        const OFFSET_X = 300;
+        const OFFSET_Y = 120;
+        const INIT_X = 75;
+        const INIT_Y = 15;
+        const DOCUMENT_WIDTH = this.screenWidth - 800;
         let r = 0;
         let c = 0;
 
         for (let i = 0; i < this.ids.length; i++) {
-            const item = this.ids[i];
-            const l = initX + (c * offsetX);
-            const t = initY + (r * offsetY);
-            item.left = l;
-            item.top = t;
+            const ITEM = this.ids[i];
+            const LEFT = INIT_X + (c * OFFSET_X);
+            const TOP = INIT_Y + (r * OFFSET_Y);
+            ITEM.left = LEFT;
+            ITEM.top = TOP;
             c++;
-            if (((c + 1) * offsetX) > docWidth) {
+            if (((c + 1) * OFFSET_X) > DOCUMENT_WIDTH) {
                 c = 0;
                 r++;
             }
-            item.position = { x: l, y: t }; 
+            ITEM.position = { x: LEFT, y: TOP }; 
         }
         this.save();
-        this.brdcstservice.dispatch(new AppEvent("QuickDealShowPanel", "showpanel"));
+        this.broadcastService.dispatch(new AppEvent("QuickDealShowPanel", "showpanel"));
     }
 
     ngOnInit() { 
