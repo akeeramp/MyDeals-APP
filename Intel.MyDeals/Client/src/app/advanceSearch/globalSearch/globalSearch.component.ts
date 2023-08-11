f1335b63ead8786d0c5c8c1633a686b10d98c1bc
Jesus Lopez Reynosa <jesus.lopez.reynosa@intel.com>
2023-08-11 09:54:05 -0700

    TWC3119-712: Add Input Sanitization to Global Search to prevent errors (#2429)
    * TWC3119-712: Added sanitization to Global Search input
    
    * TWC3119-712: Updated regex to include acceptable special characters that do not cause an API error

--- Intel.MyDeals/Client/src/app/advanceSearch/globalSearch/globalSearch.component.ts
+++ Intel.MyDeals/Client/src/app/advanceSearch/globalSearch/globalSearch.component.ts
@@ -1,87 +1,87 @@
-import {Component,ViewChild } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { broadCastService } from "../../core/dealPopup/broadcast.service";
 import { GlobalSearchResultsComponent } from "../globalSearchResults/globalSearchResults.component";
 
 @Component({
-  selector: "global-search-angular",
-  templateUrl: "Client/src/app/advanceSearch/globalSearch/globalSearch.component.html",
-  styleUrls:["Client/src/app/advanceSearch/globalSearch/globalSearch.component.css"],
-  
+  selector: 'global-search-angular',
+  templateUrl: 'Client/src/app/advanceSearch/globalSearch/globalSearch.component.html',
+  styleUrls:['Client/src/app/advanceSearch/globalSearch/globalSearch.component.css']
 })
-
 export class GlobalSearchComponent  {
-  //for calling Child function from Parent
-  @ViewChild(GlobalSearchResultsComponent) GlobalSearchResults: GlobalSearchResultsComponent; 
-    private searchText="";
-    private opType="ALL";
-    private searchDialogVisible=false;
-    private resultTake=5;
-    private windowOpened= false;
+    //for calling Child function from Parent
+    @ViewChild(GlobalSearchResultsComponent) GlobalSearchResults: GlobalSearchResultsComponent; 
+    private searchText = "";
+    private opType = "ALL";
+    private searchDialogVisible = false;
+    private resultTake = 5;
+    private windowOpened = false;
     private windowTop = 75; windowLeft = 300; windowWidth = 950; windowHeight = 500; windowMinWidth = 100; windowMinHeight = 100;
 
-    constructor(private brdcstservice: broadCastService) {
+    constructor(private broadcastService: broadCastService) { }
 
+    enterPressed(event: any) {
+        //KeyCode 13 is 'Enter'
+        if (event.keyCode === 13) {
+            if (this.searchText != "") {
+                //opening kendo window
+                this.searchText = this.searchText.trim();
+                this.executeOnly(this.opType);
+                this.setWindowWidth();
+                this.windowOpened = true;
+            } else {
+                this.searchDialogVisible = true;
+            }
+        }
     }
- 
-    enterPressed(event:any) {
-      //KeyCode 13 is 'Enter'
-      if (event.keyCode === 13 && this.searchText != "") {
-         //opening kendo window
-         this.searchText=this.searchText.trim();
-          this.executeOnly(this.opType);
-         this.setWindowWidth();
-         this.windowOpened=true;
-      }
-      if(event.keyCode === 13 && this.searchText == ""){
-        this.searchDialogVisible=true;
-      }
+
+    setWindowWidth() {
+        if (this.opType == "ALL") {
+            this.windowWidth = 1000;
+        } else {
+            this.windowWidth = 600;
+        }
     }
-    setWindowWidth(){
-      if(this.opType=="ALL"){
-        this.windowWidth=1000;
-      }
-      else{
-        this.windowWidth=600;
-      }
+
+    closeDialog() {
+        this.searchDialogVisible = false;
     }
-    closeDialog(){
-      this.searchDialogVisible=false;
-    }
-    executeOnly(opType:string){
+
+    executeOnly(opType: string) {
         if (this.searchText != "") {
             //opening kendo window
-            this.opType=opType;
+            this.opType = opType;
             this.setWindowWidth();
             //this condition is required since this should work only if kendo window is open 
             if (this.GlobalSearchResults) {
                 this.GlobalSearchResults.onOpTypeChange(this.opType);
                 this.windowOpened = true;
-            }
-            else {
+            } else {
                 this.windowOpened = true;
             }
-        }
-        else {
+        } else {
             this.searchDialogVisible = true;
         }
     }
+
     windowClose() {
-      this.windowOpened = false;
+        this.windowOpened = false;
     }
-    //these methond is an output method from globaslsearchresult component
-    getWindowWidth($event:number){
-      this.windowWidth=$event;
+
+    //these methods are output methods from GlobalSearchResultsComponent
+    getWindowWidth($event: number) {
+        this.windowWidth = $event;
     }
-    isWindowOpen($event:boolean){
+
+    isWindowOpen($event: boolean) {
         this.windowOpened = $event;
         this.searchText = "";
     }
 
     ngOnInit() {
-        
-        this.brdcstservice.on("contractSearch").subscribe(event => {
+        this.broadcastService.on("contractSearch").subscribe(event => {
             this.searchText = event.payload.searchText;
             this.opType = event.payload.opType;
+
             if (event.payload.event == "selectedValue") {
                 this.executeOnly(event.payload.opType);
             } else {
@@ -90,5 +90,3 @@ export class GlobalSearchComponent  {
         });
     }
 }
-
-
--- Intel.MyDeals/Client/src/app/advanceSearch/globalSearchResults/globalSearchResults.component.ts
+++ Intel.MyDeals/Client/src/app/advanceSearch/globalSearchResults/globalSearchResults.component.ts
@@ -1,153 +1,144 @@
+/* eslint-disable no-useless-escape */
 import { Component, EventEmitter, Input, Output, ChangeDetectorRef } from "@angular/core";
 import { globalSearchResultsService } from "./globalSearchResults.service";
 import { logger } from "../../shared/logger/logger";
 
 @Component({
-  selector: "global-search-results-angular",
-  templateUrl: "Client/src/app/advanceSearch/globalSearchResults/globalSearchResults.component.html",
-  styleUrls:["Client/src/app/advanceSearch/globalSearchResults/globalSearchResults.component.css"]
+  selector: 'global-search-results-angular',
+  templateUrl: 'Client/src/app/advanceSearch/globalSearchResults/globalSearchResults.component.html',
+  styleUrls:['Client/src/app/advanceSearch/globalSearchResults/globalSearchResults.component.css']
 })
-export class GlobalSearchResultsComponent  {
-    constructor(protected globalSearchSVC: globalSearchResultsService, private loggerSvc: logger, private ref: ChangeDetectorRef) {}
+export class GlobalSearchResultsComponent {
+
+    constructor(protected globalSearchService: globalSearchResultsService,
+                private loggerService: logger,
+                private ref: ChangeDetectorRef) {}
+
     //these are input coming from gloablsearch component
     @Input() searchText = "";
-    response :any
-    @Input() opType="ALL";
-    @Output() getWindowWidth = new EventEmitter;  
-    @Output() isWindowOpen=new EventEmitter;
-    private resultTake=5;
+    response: any
+    @Input() opType = "ALL";
+    @Output() getWindowWidth = new EventEmitter;
+    @Output() isWindowOpen = new EventEmitter;
+    private resultTake = 5;
     private viewMoreVisible = true;
-    public isLoading:boolean;
-    private opTypes:Array<any> = [
-      {
-          value: "ALL",
-          label: "All"
-      },
-      {
-          value: "CNTRCT",
-          label: "Contract"
-      },
-      {
-          value: "PRC_ST",
-          label: "Pricing Strategy"
-      },
-      {
-          value: "PRC_TBL",
-          label: "Pricing Table"
-      },
-      {
-          value: "WIP_DEAL",
-          label: "Deals"
-      }
-    ]
-    private objTypes:any={
-      'CNTRCT':{result:[],loading:true,viewMore:false},
-      'PRC_ST':{result:[],loading:true,viewMore:false},
-      'PRC_TBL':{result:[],loading:true,viewMore:false},
-      'WIP_DEAL':{result:[],loading:true,viewMore:false}
+    public isLoading: boolean;
+    private readonly opTypes: Array<any> = [
+        { value: "ALL", label: "All" },
+        { value: "CNTRCT", label: "Contract" },
+        { value: "PRC_ST", label: "Pricing Strategy" },
+        { value: "PRC_TBL", label: "Pricing Table" },
+        { value: "WIP_DEAL", label: "Deals" }
+    ];
+    private objTypes: any = {
+        'CNTRCT': { result:[], loading:true, viewMore:false },
+        'PRC_ST': { result:[], loading:true, viewMore:false },
+        'PRC_TBL': { result:[], loading:true, viewMore:false },
+        'WIP_DEAL': { result:[], loading:true, viewMore:false }
     };
 
-    getOBJonly(type:string){
-      this.objTypes[type].loading=true;
-      this.objTypes[type].viewMore=false;
-      this.globalSearchSVC.getObjectType(this.searchText,this.resultTake,type).subscribe(Result =>{
-          this.objTypes[type].result=Result;
-          this.objTypes[type].loading=false;
-          //this method is added for UI to render proper. without this line the UI databinding is not happening from dashboard search screen but it will work fine for header search
-          this.ref.detectChanges();
-        if(this.objTypes[type].result.length==5){
-          this.objTypes[type].viewMore=true;
+    getObjectOnly(type: string) {
+        this.objTypes[type].loading = true;
+        this.objTypes[type].viewMore = false;
+
+        const sanitizedSearchText: string = this.searchText.replace(/[^a-zA-Z0-9\(\)\-\_\@ ]/g, '');  // Allow rule: alphanumeric, space, -, _, @, (, )
+        this.globalSearchService.getObjectType(sanitizedSearchText, this.resultTake, type).subscribe((result) => {
+            this.objTypes[type].result = result;
+            this.objTypes[type].loading = false;
+            //this method is added for UI to render proper. without this line the UI databinding is not happening from dashboard search screen but it will work fine for header search
+            this.ref.detectChanges();
+            if (this.objTypes[type].result.length == 5) {
+                this.objTypes[type].viewMore = true;
+            }
+        }, (err) => {
+            this.loggerService.error("Something went wrong.", "Error", err);
+        });
+    }
+
+    getObjectTypeResult(opType: string) {
+        //setting loading to default true
+        if (opType == "ALL") {
+            this.getObjectOnly('CNTRCT');
+            this.getObjectOnly('PRC_ST');
+            this.getObjectOnly('PRC_TBL');
+            this.getObjectOnly('WIP_DEAL');
+        } else if (opType == "CNTRCT") {
+            this.getObjectOnly('CNTRCT');
+        } else if (opType == "PRC_ST") {
+            this.getObjectOnly('PRC_ST');
+        } else if (opType == "PRC_TBL") {
+            this.getObjectOnly('PRC_TBL');
+        } else {
+            this.getObjectOnly('WIP_DEAL');
         }
-      },err=>{
-          this.loggerSvc.error("Something went wrong.","Error",err);
-      })
     }
-    getObjectTypeResult(opType:string){
-      //setting loading to default true
-       if(opType=="ALL"){
-        this.getOBJonly('CNTRCT');
-        this.getOBJonly('PRC_ST');
-        this.getOBJonly('PRC_TBL');
-        this.getOBJonly('WIP_DEAL');
-       }
-       else if(opType=="CNTRCT"){
-        this.getOBJonly('CNTRCT');
-       }
-       else if(opType=="PRC_ST"){
-        this.getOBJonly('PRC_ST');
-       }
-       else if(opType=="PRC_TBL"){
-        this.getOBJonly('PRC_TBL');
-       }
-       else {
-        this.getOBJonly('WIP_DEAL');
-       }
+
+    txtEnterPressed(event: any) {
+        //KeyCode 13 is 'Enter'
+        if (event.keyCode === 13 && this.searchText != "") {
+            //opening kendo window
+            this.getObjectTypeResult(this.opType);
+        }
     }
-    txtEnterPressed(event:any) {
-      //KeyCode 13 is 'Enter'
-      if (event.keyCode === 13 && this.searchText != "") {
-        //opening kendo window
-         this.getObjectTypeResult(this.opType);
-      }
+
+    windowResize() {
+        if (this.opType == "ALL") {
+            this.getWindowWidth.emit(950);
+        } else {
+            this.getWindowWidth.emit(565);
+        }
     }
-    windowResize(){
-      if(this.opType =="ALL"){
-        this.getWindowWidth.emit(950);
-      }
-      else{
-        this.getWindowWidth.emit(565);
-      }
+
+    onOpTypeChange(opType: string) {
+        if (this.searchText != "") {
+            this.opType = opType;
+            this.resultTake = 5;
+            this.windowResize();
+            this.getObjectTypeResult(this.opType);
+        } else {
+            this.loggerService.warn("Please Enter: (1) Contract/ Pricing Strategy / Pricing Table Name or Number OR (2) Deal Number.", "");
+        }
     }
-    onOpTypeChange(opType:string) {
-      if (this.searchText != "") {
-        this.opType=opType;
-        this.resultTake=5;
-        this.windowResize();
-        this.getObjectTypeResult(this.opType);
-      }
-      else{
-          this.loggerSvc.warn("Please Enter: (1) Contract/ Pricing Strategy / Pricing Table Name or Number OR (2) Deal Number.","");
-      }
-    }
-    gotoOBJ(item: any, opType: string) {
+
+    gotoObject(item: any, opType: string) {
         $("body").removeClass("z-index-zero");
-        let DCID = item.DC_ID
+        let dcId = item.DC_ID
         this.isWindowOpen.emit(false);
-        if (DCID <= 0) {
-            this.loggerSvc.error("Unable to locate the Pricing Strategy.", "error")
+
+        if (dcId <= 0) {
+            this.loggerService.error("Unable to locate the Pricing Strategy.", "error")
             return;
         }
-        if (opType == 'CNTRCT' ) {
-          window.location.href = "Contract#/manager/CNTRCT/" + DCID + "/0/0/0";
-        }
-        else if (opType == 'PRC_ST' || opType == 'PRC_TBL' || opType == 'WIP_DEAL') {          
+
+        if (opType == 'CNTRCT') {
+            window.location.href = "Contract#/manager/CNTRCT/" + dcId + "/0/0/0";
+        } else if (opType == 'PRC_ST' || opType == 'PRC_TBL' || opType == 'WIP_DEAL') {
             //calling this function because to navigate to the PS we need contract data,PS ID and PT ID -- in the item we dont have PT ID for opType ->PS so hitting API to get data
             //in case of WIp deal click on the global search results we need contract id ,PS and PT ID to navigate to respective deal so calling this function to hit the api to get the details
             //in case of PT ID click on the global search results we need contract ID which is not present in item so calling API to get the data
-            this.getIDs(DCID, item.DC_PARENT_ID, opType)
+            this.getIds(dcId, item.DC_PARENT_ID, opType)
         }
     }
 
-    viewMore(opType:string){
-      this.resultTake=50;
-      this.getObjectTypeResult(opType);
+    viewMore(opType: string) {
+        this.resultTake = 50;
+        this.getObjectTypeResult(opType);
     }
 
-    getIDs(dcId, parentdcID, opType = "") {
-      this.isLoading = true;
-        this.globalSearchSVC.getContractIDDetails(dcId,opType).subscribe(res => {
-            this.isLoading = false;
-            if (res) {
-                this.response = res;
-                if (opType == "WIP_DEAL")
-                    window.location.href = "Contract#/gotoDeal/" + dcId;
-                else if (opType == "PRC_ST")
-                    window.location.href = "Contract#/gotoPs/" + this.response.PricingStrategyId;
-                else window.location.href = "Contract#/manager/PT/" + this.response.ContractId + "/" + parentdcID + "/" + dcId + "/0";
-            }
-        },
-            error => {
-                this.loggerSvc.error("GlobalSearchResultsComponent::getContractIDDetails::Unable to get Contract Data", error);
+    getIds(dcId, parentDcId, opType = "") {
+        this.isLoading = true;
+        this.globalSearchService.getContractIDDetails(dcId, opType).subscribe((res) => {
+                this.isLoading = false;
+                if (res) {
+                    this.response = res;
+                    if (opType == "WIP_DEAL")
+                        window.location.href = "Contract#/gotoDeal/" + dcId;
+                    else if (opType == "PRC_ST")
+                        window.location.href = "Contract#/gotoPs/" + this.response.PricingStrategyId;
+                    else window.location.href = "Contract#/manager/PT/" + this.response.ContractId + "/" + parentDcId + "/" + dcId + "/0";
+                }
+            }, (error) => {
+                this.loggerService.error("GlobalSearchResultsComponent::getContractIDDetails::Unable to get Contract Data", error);
                 this.isLoading = false;
             }
         );
@@ -158,6 +149,7 @@ export class GlobalSearchResultsComponent  {
         this.isWindowOpen.emit(false);
         window.location.href = "AdvancedSearch#/advanceSearch";
     }
+
     ngOnInit() {
         this.getObjectTypeResult(this.opType);
     }
