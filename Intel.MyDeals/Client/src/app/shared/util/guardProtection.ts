88f2ce06b9dfeb7a659c8c5ba361cc4c7ff7aba0
anurag-i98 <99398864+anurag-i98@users.noreply.github.com>
2024-07-30 12:16:23 +0530

    TWC3179-4112: Role Access logic Modification with proper bit logic (#2967)

--- Intel.MyDeals/Client/src/app/shared/util/guardProtection.ts
+++ Intel.MyDeals/Client/src/app/shared/util/guardProtection.ts
@@ -12,56 +12,39 @@ export class authGuard implements CanActivate {
     constructor(private router: Router) {}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         //getting role details
-        let role = (<any>window).usrRole.toUpperCase();
-        let bit = '';
-        let prefix = '';
-
-        if ((<any>window).isSuper)
-            bit += 'Super';
-        if ((<any>window).isTester)
-            bit += 'T';
-        if ((<any>window).isBulkPriceAdmin)
-            bit += 'B';
-        if ((<any>window).isDeveloper)
-            bit += 'D';
-        if ((<any>window).isCustomerAdmin && (<any>window).usrRole == 'SA')
-            prefix = 'Account';
-
-        role = bit.length > 0 ? (role + '-' + bit) : role;
-        role = prefix.length > 0 ? prefix + ' ' + role : role;
+        const userRole = (<any>window).usrRole.toUpperCase();
+        const roleBits = {
+            'Super': (<any>window).isSuper,
+            'T': (<any>window).isTester,
+            'B': (<any>window).isBulkPriceAdmin,
+            'D': (<any>window).isDeveloper
+        };
+        const roleCombinations = this.getRoleCombinations(userRole, roleBits);
 
         let path = route.routeConfig.path;
         path = route.queryParams.manageType && route.queryParams.manageType == 'pctDiv' ? path + `?loadtype=${route.queryParams.loadtype}&manageType=${route.queryParams.manageType}` : path;
-
-        if (guardConfig.guardSettings[path][role] == undefined) {
-            //checking for special conditions
-            if (role.includes('T')) {
-                if (guardConfig.guardSettings[path][(<any>window).usrRole.toUpperCase() + '-T']) return true;
+        
+        for (const role of roleCombinations) {
+            if (guardConfig.guardSettings[path] && guardConfig.guardSettings[path][role]) {
+                return true;
             }
-            if (role.includes('D')) {
-                if (guardConfig.guardSettings[path][(<any>window).usrRole.toUpperCase() + '-D']) return true;
-            }
-            if (role.includes('B')) {
-                if (guardConfig.guardSettings[path][(<any>window).usrRole.toUpperCase() + '-B']) return true;
-            }
-            if (role.includes('Super')) {
-                if (guardConfig.guardSettings[path][(<any>window).usrRole.toUpperCase() + '-Super']) return true;
-            }
-            if (role.includes('Account')) {
-                if (guardConfig.guardSettings[path]['Account ' + (<any>window).usrRole.toUpperCase()]) return true;
-            }
-            else {
-                window.alert("User does not have access to the screen. Press OK to redirect to Dashboard.");
-                window.location.href = 'Dashboard#/portal';
-            }
-        } else if (guardConfig.guardSettings[path][role] != undefined && guardConfig.guardSettings[path][role])
-            return true;
-        else {
-            window.alert("User does not have access to the screen. Press OK to redirect to Dashboard.");
-            window.location.href = 'Dashboard#/portal';
-
         }
-
+        
+        window.alert("User does not have access to the screen. Press OK to redirect to Dashboard.");
+        window.location.href = 'Dashboard#/portal';
+        return false;
     }
 
+    private getRoleCombinations(baseRole: string, roleBits: { [key: string]: boolean }): string[] {
+        const combinations = [baseRole];
+        for (const [bit, hasBit] of Object.entries(roleBits)) {
+            if (hasBit) {
+                combinations.push(`${baseRole}-${bit}`);
+            }
+        }
+        if ((<any>window).isCustomerAdmin && baseRole === 'SA') {
+            combinations.push('Account SA');
+        }
+        return combinations;
+    }
 }
